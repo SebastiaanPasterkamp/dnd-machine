e523bc42da0bddd0f7af97da86aad9bdd397a879
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2017-09-15 20:29:50 +0200

    Run: Added CLI arguments

--- run.py
+++ run.py
@@ -1,4 +1,27 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from dndmachine import app
-app.run(host='0.0.0.0', port=5000, debug=True)
+from dndmachine.app import app
+from optparse import OptionParser
+
+parser = OptionParser("""D&D Machine Web App""")
+
+parser.add_option("--host", dest="host", default="127.0.0.1",
+                  metavar="HOST",
+                  help="Bind to IP or Host [default %default]")
+
+parser.add_option("--port", dest="port", default=8080,
+                  metavar="PORT", type=int,
+                  help="Use this port [default %default]")
+
+parser.add_option("--debug", dest="debug", default=False,
+                  action="store_false",
+                  help="Enable debugging [default: %default]")
+
+(options, args) = parser.parse_args()
+
+app.run(
+    host=options.host,
+    port=options.port,
+    debug=options.debug
+    )
