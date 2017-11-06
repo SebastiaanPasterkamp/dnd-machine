150fa076ee15f36996af3485377615069fcf1b1d
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2017-11-06 21:11:27 +0100

    Rjs: WIP Switched /items to React, adding more views

--- dndmachine/source/jsx/index.jsx
+++ dndmachine/source/jsx/index.jsx
@@ -4,8 +4,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import LanguageTable from './views/Languages.jsx';
-import WeaponsTable from './views/Weapons.jsx';
+import ArmorTable from './views/ArmorTable.jsx';
+import LanguageTable from './views/LanguagesTable.jsx';
+import NpcEdit from './views/NpcEdit.jsx';
+import SpellsTable from './views/SpellsTable.jsx';
+import WeaponsTable from './views/WeaponsTable.jsx';
 import DefaultFilter from './views/DefaultFilter.jsx';
 
 let app = document.getElementById('app'),
@@ -15,8 +18,11 @@ if (app) {
     ReactDom.render(
         <Router>
             <Switch>
+                <Route path="/items/armor" component={ArmorTable} />
                 <Route path="/items/languages" component={LanguageTable} />
+                <Route path="/items/spells" component={SpellsTable} />
                 <Route path="/items/weapons" component={WeaponsTable} />
+                <Route path="/npc/edit" component={NpcEdit} />
             </Switch>
         </Router>,
         app
@@ -29,3 +35,4 @@ if (filter) {
         filter
     );
 }
+
