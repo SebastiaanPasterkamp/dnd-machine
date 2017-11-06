1e2af3d7a81890442c0e1f8e0f8087599a987ea4
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2017-11-06 22:27:58 +0100

    ReactJS: First set of component tests w/ snapshots

---
+++ dndmachine/source/tests/components/Damage.react-test.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Damage from '../../jsx/components/Damage.jsx';
+import renderer from 'react-test-renderer';
+
+describe('Component: Damage', () => {
+    it('should show simple dice notation', () => {
+        const props = {
+            dice_count: 1,
+            dice_size: 4
+        };
+        const tree = renderer.create(
+            <Damage {...props} />
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should show dice notation with bonus', () => {
+        const props = {
+            dice_count: 2,
+            dice_size: 6,
+            dice_bonus: 3
+        };
+        const tree = renderer.create(
+            <Damage {...props} />
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should show dice notation with type', () => {
+        const props = {
+            dice_count: 3,
+            dice_size: 8,
+            type: 'fire'
+        };
+        const tree = renderer.create(
+            <Damage {...props} />
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should show dice notation with bonus and type', () => {
+        const props = {
+            dice_count: 4,
+            dice_size: 10,
+            dice_bonus: 6,
+            type: 'piercing'
+        };
+        const tree = renderer.create(
+            <Damage {...props} />
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
