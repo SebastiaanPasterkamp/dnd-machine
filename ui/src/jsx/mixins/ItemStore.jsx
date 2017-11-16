611e516f5a844c721a53f35d8d817c85ce646b34
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2017-11-16 19:52:03 +0100

    ReactJS: ItemSTore composition mixin

---
+++ ui/src/jsx/mixins/ItemStore.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Reflux from 'reflux';
+import _ from 'lodash';
+
+import listDataActions from '../actions/listDataActions.jsx';
+import DataStore from '../stores/dataStore.jsx';
+
+class ItemStore extends Reflux.Component
+{
+    constructor(props) {
+        super(props);
+        this.store = DataStore;
+        this.storeKeys = this.props.itemStoreProps;
+    }
+
+    componentDidMount() {
+        this.props.itemStoreProps.map((item) => {
+            if (_.isEmpty(this.state[item])) {
+                listDataActions.fetchItems(item);
+            }
+        });
+    }
+
+    render() {
+        return <this.props.component
+            {...this.state}
+            {...this.props}
+            />
+    }
+}
+
+export default ItemStore;
