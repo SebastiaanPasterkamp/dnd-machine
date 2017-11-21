82b5387235e7c6931a4d8341813ee1c45a6c6fc7
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2017-11-21 22:00:36 +0100

    ReactJS: Switched to near-full ReactJS, incl navigation

--- ui/src/jsx/index.jsx
+++ ui/src/jsx/index.jsx
@@ -2,8 +2,11 @@
 
 import React from 'react';
 import ReactDom from 'react-dom';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
 
+import ItemStore from './mixins/ItemStore.jsx';
+
+import ArmorEdit from './views/ArmorEdit.jsx';
 import ArmorTable from './views/ArmorTable.jsx';
 import LanguageTable from './views/LanguagesTable.jsx';
 import CharacterEdit from './views/CharacterEdit.jsx';
@@ -11,15 +14,45 @@ import NpcEdit from './views/NpcEdit.jsx';
 import WeaponEdit from './views/WeaponEdit.jsx';
 import SpellsTable from './views/SpellsTable.jsx';
 import WeaponsTable from './views/WeaponsTable.jsx';
+
 import DefaultFilter from './views/DefaultFilter.jsx';
+import Navigation from './views/Navigation.jsx';
 
 let app = document.getElementById('app'),
-    filter = document.getElementById('default-filter');
+    filter = document.getElementById('default-filter'),
+    navigation = document.getElementById('navigation');
 
-if (app) {
-    ReactDom.render(
-        <Router>
+ReactDom.render(
+    <Router><div>
+        <header className="nice-header fixed">
+            <div className="nice-header-container">
+                <h1 className="nice-header-brand">
+                    <Link to="/">
+                        <img
+                            src="/static/img/dungeons-and-dragons-logo.png"
+                            height="35"
+                            />
+                    </Link>
+                </h1>
+                <div className="nice-header-collapse">
+
+                    <DefaultFilter/>
+
+                    <Navigation />
+                </div>
+
+                <button type="button" className="nice-header-toggle collapsed">
+                    <span className="icon-bar top-bar"></span>
+                    <span className="icon-bar middle-bar"></span>
+                    <span className="icon-bar bottom-bar"></span>
+                </button>
+
+            </div>
+        </header>
+        <section className="nice-fluid-container grid">
             <Switch>
+                <Route path="/items/armor/new" component={ArmorEdit} />
+                <Route path="/items/armor/edit/:id" component={ArmorEdit} />
                 <Route path="/items/armor" component={ArmorTable} />
                 <Route path="/items/languages" component={LanguageTable} />
                 <Route path="/items/spells" component={SpellsTable} />
@@ -28,16 +61,21 @@ if (app) {
                 <Route exact path="/items/weapons" component={WeaponsTable} />
                 <Route path="/npc/edit/:id" component={NpcEdit} />
                 <Route path="/character/edit/:id" component={CharacterEdit} />
+                <Route render={({location}) => {
+                    window.location.href = location.pathname;
+                    return <div className="nice-modal info viewport-center">
+                        <div className="nice-modal-content">
+                            <div className="nice-modal-header">
+                                <h4>Redirecting</h4>
+                            </div>
+                            <div className="nice-modal-body">
+                                Redirecting to <code>{location.pathname}</code>.
+                            </div>
+                        </div>
+                    </div>;
+                }} />
             </Switch>
-        </Router>,
-        app
-    );
-}
-
-if (filter) {
-    ReactDom.render(
-        <DefaultFilter/>,
-        filter
-    );
-}
-
+        </section>
+    </div></Router>,
+    document.getElementById('app')
+);
---
+++ ui/src/jsx/views/Navigation.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import LazyComponent from '../components/LazyComponent.jsx';
+import ItemStore from '../mixins/ItemStore.jsx';
+
+class Navigation extends LazyComponent
+{
+    renderItem(item) {
+        return <li key={item.label} className="highlight">
+            <Link
+                to={item.path}
+                className={"icon fa-" + item.icon}>
+                {item.label}
+            </Link>
+        </li>;
+    }
+
+    renderItemGroup(group) {
+        return <li key={group.label}>
+            <span className={"icon fa-" + group.icon}>
+                {group.label}
+            </span>
+            <ul>
+                {group.items.map((item) => {
+                    return this.renderItem(item);
+                })}
+            </ul>
+        </li>;
+    }
+
+    render() {
+        const navigation = this.props.navigation || [];
+
+        return <ul className="nice-header-menu menu-pills">
+            {navigation.map((nav) => {
+                return 'items' in nav
+                    ? this.renderItemGroup(nav)
+                    : this.renderItem(nav);
+            })}
+        </ul>;
+    }
+}
+
+export default ItemStore(Navigation, ['navigation'], '.');
