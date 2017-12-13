a79478f344a5385be803a54205acd76d95898fe6
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2017-12-13 21:52:10 +0100

    ReactJS: Added InlineDataWrapper HoC, like ListDataWrapper, but adds only 1 as direct props

---
+++ ui/src/jsx/hocs/InlineDataWrapper.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Reflux from 'reflux';
+import _ from 'lodash';
+
+import ListDataActions from '../actions/ListDataActions.jsx';
+import ListDataStore from '../stores/ListDataStore.jsx';
+
+function InlineDataWrapper(WrappedComponent, storeKey, storeCategory=null) {
+
+    return class extends Reflux.Component {
+        constructor(props) {
+            super(props);
+            this.state = {}
+            this.store = ListDataStore;
+            this.storeKeys = [storeKey];
+        }
+
+        componentDidMount() {
+            if (storeKey in this.state && this.state[storeKey] != null) {
+                return;
+            }
+
+            ListDataActions.fetchItems(
+                storeKey, storeCategory);
+        }
+
+        shouldComponentUpdate(nextProps, nextState) {
+            if (!_.isEqual(this.props, nextProps)) {
+                return true;
+            }
+
+            if (!_.isEqual(this.state, nextState)) {
+                return true;
+            }
+
+            return false;
+        }
+
+        render() {
+            if (!(storeKey in this.state)) {
+                return null;
+            }
+            return <WrappedComponent
+                {...this.props}
+                {...this.state[storeKey]}
+                />
+        }
+    };
+}
+
+export default InlineDataWrapper;
