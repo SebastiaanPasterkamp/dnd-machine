0035e747202c29ee2bb69ff96051bfb346ebacd1
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2017-12-13 21:49:34 +0100

    ReactJS: ListDataActions - added login/logout actions, updating 'current_user'

--- ui/src/jsx/actions/ListDataActions.jsx
+++ ui/src/jsx/actions/ListDataActions.jsx
@@ -4,6 +4,8 @@ import _ from 'lodash';
 
 var ListDataActions = Reflux.createActions({
     "setState": {},
+    "doLogin": {children: ['completed', 'failed']},
+    "doLogout": {children: ['completed', 'failed']},
     "fetchItems": {children: ['completed', 'failed']},
 });
 let throttledGet = {};
@@ -21,9 +23,13 @@ ListDataActions.fetchItems.listen((type, category=null) => {
                 }
             })
             .then((response) => response.json())
-            .then((data) => {
+            .then((response) => {
+                if (response && 'error' in response) {
+                    ListDataActions.fetchItems.failed(response.error);
+                    return false;
+                }
                 let update = [];
-                update[type] = data;
+                update[type] = response;
                 ListDataActions.fetchItems.completed(update);
             })
             .catch((error) => {
@@ -34,6 +40,57 @@ ListDataActions.fetchItems.listen((type, category=null) => {
     }
 
     throttledGet[path](path, type);
+});
+
+ListDataActions.doLogin.listen((credentials, success, failure) => {
+    fetch('/login', {
+        credentials: 'same-origin',
+        method: 'POST',
+        'headers': {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if (response && 'error' in response) {
+            ListDataActions.doLogin.failed(response.error);
+            return false;
+        }
+        ListDataActions.fetchItems.completed({'current_user': response});
+        if (success) {
+            success(response);
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        ListDataActions.doLogin.failed(error);
+        if (failure) {
+            failure(error);
+        }
+    });
+});
+
+ListDataActions.doLogout.listen(() => {
+    fetch('/logout', {
+        credentials: 'same-origin',
+        'headers': {
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if (response && 'error' in response) {
+            ListDataActions.doLogout.failed(response.error);
+            return false;
+        }
+        ListDataActions.fetchItems.completed({'current_user': response});
+    })
+    .catch((error) => {
+        console.error(error);
+        ListDataActions.doLogout.failed(error);
+    });
 });
 
 export default ListDataActions;
