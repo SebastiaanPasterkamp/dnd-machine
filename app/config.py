1e9e907c21a727778f18397a6ec3abf25cbe7f0a
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-01-09 20:37:13 +0100

    App: get_character_data can skip importing .md files

--- app/config.py
+++ app/config.py
@@ -12,16 +12,18 @@ def get_config():
             config = json.load(cfg)
     return config
 
-def get_character_data():
+def get_character_data(withMarkdown=False):
     with open(os.path.join('app', 'character-data.json')) as cfg:
         character_data = json.load(cfg)
+
+    if withMarkdown:
         for section in ['race', 'class', 'background']:
             for part in character_data[section]:
-                mdfile = os.path.join(
-                    'app', 'data', part['filename'])
+                mdfile = os.path.join('app', 'data', part['filename'])
                 with codecs.open(mdfile, encoding='utf-8') as fh:
                     part['description'] = fh.read()
-        return character_data
+
+    return character_data
 
 def get_npc_data():
     with open(os.path.join('app', 'npc-data.json')) as cfg:
