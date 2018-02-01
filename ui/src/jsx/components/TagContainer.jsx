09eae5247e720a82750747f789993c42c6a1004a
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-02-01 22:06:51 +0100

    ReactJS: TagContainer gets PropTypes, and onX callbacks

--- ui/src/jsx/components/TagContainer.jsx
+++ ui/src/jsx/components/TagContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 import BaseTagContainer from './BaseTagContainer.jsx';
@@ -6,25 +7,38 @@ import BaseTagContainer from './BaseTagContainer.jsx';
 export class TagContainer extends BaseTagContainer
 {
     onChange(key, value) {
-        let head = _.take(
-                this.props.tags, key),
-            tail = _.takeRight(
-                this.props.tags, this.props.tags.length - key - 1);
-        this.props.setState(head.concat([value]).concat(tail));
+        const {tags, onChange} = this.props;
+        const head = _.take(tags, key);
+        const tail = _.takeRight(tags, tags.length - key - 1);
+        this.props.setState(
+            head.concat([value]).concat(tail)
+        );
+        if (onChange) {
+            onChange(key, value);
+        }
     }
 
     onDelete(key, value) {
-        let head = _.take(
-                this.props.tags, key),
-            tail = _.takeRight(
-                this.props.tags, this.props.tags.length - key - 1);
-        this.props.setState(head.concat(tail));
+        const {tags, onDelete} = this.props;
+        const head = _.take(tags, key);
+        const tail = _.takeRight(tags, tags.length - key - 1);
+        this.props.setState(
+            head.concat(tail)
+        );
+        if (onDelete) {
+            onDelete(key, value);
+        }
     }
 
     onAdd(value) {
-        let head = _.take(
-                this.props.tags, this.props.tags.length);
-        this.props.setState(head.concat([value]));
+        const {tags, onAdd} = this.props;
+        const head = _.take(tags, tags.length);
+        this.props.setState(
+            head.concat([value])
+        );
+        if (onAdd) {
+            onAdd(value);
+        }
     }
 
     getItem(key, value) {
@@ -39,6 +53,13 @@ TagContainer.defaultProps = {
     setState: (value) => {
         console.log(['TagContainer', value]);
     }
+};
+
+TagContainer.propTypes = {
+    tagOptions: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onAdd: PropTypes.func,
+    onChange: PropTypes.func,
+    onDelete: PropTypes.func,
 };
 
 export default TagContainer;
