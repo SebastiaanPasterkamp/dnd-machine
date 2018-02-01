2fa5aa55568c8fd7d4cb2a8f88b94f2d89383ed2
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-02-01 22:04:34 +0100

    App: Armor/Weapon items use 'type' from configuration

--- app/models/item/armor.py
+++ app/models/item/armor.py
@@ -3,7 +3,7 @@ from ..base import JsonObject, JsonObjectDataMapper
 class ArmorObject(JsonObject):
     _pathPrefix = "armor"
     _defaultConfig = {
-        "type": u"light armor",
+        "type": u"light",
         "name": u"",
         "cost": {},
         "disadvantage": False,
@@ -13,6 +13,9 @@ class ArmorObject(JsonObject):
         "id": int,
         "cost": {
             "*": int
+            },
+        "weight": {
+            "*": float
             },
         "disadvantage": bool,
         "value": int,
@@ -25,8 +28,7 @@ class ArmorMapper(JsonObjectDataMapper):
     fields = ["type", "name"]
 
     def getMultiple(self, where="1", values={}):
-        where = "(%s) AND `type` LIKE :extra" % where
-        values.update({"extra": "%armor"})
+        where = "(%s) AND `type` IN ('light', 'medium', 'heavy', 'shield')" % where
         return super(ArmorMapper, self).getMultiple(
             where=where, values=values
             )
