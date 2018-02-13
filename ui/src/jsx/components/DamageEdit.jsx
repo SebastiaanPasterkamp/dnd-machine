db35e254344f904430fb3c118e898d090ff2da03
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-02-13 23:34:00 +0100

    ReactJS: Fixed DamageEdit bonus=null

--- ui/src/jsx/components/DamageEdit.jsx
+++ ui/src/jsx/components/DamageEdit.jsx
@@ -14,62 +14,64 @@ export class DamageEdit extends LazyComponent
     constructor(props) {
         super(props);
 
-        this.dice_count = _.range(0, 20)
-            .map((count) => {
-                return {
-                    code: count,
-                    label: count
-                };
-            });
-        this.dice_size = [4, 6, 8, 10, 12].map((size) => {
-            return {
-                code: size,
-                label: size
-            };
-        });
+        this.dice_count = _.range(0, 20).map((count) => ({
+            code: count,
+            label: count
+        }));
+        this.dice_size = [4, 6, 8, 10, 12].map((size) => ({
+            code: size,
+            label: size
+        }));
     }
 
     onFieldChange(field, value) {
-        let state = {
-            dice_count: this.props.dice_count,
-            dice_size: this.props.dice_size,
-            bonus: this.props.bonus,
-            type: this.props.type,
-        };
-        state[field] = value;
+        const { dice_count, dice_size, bonus, type } = this.props;
+        const state = _.assign(
+            {},
+            { dice_count, dice_size, bonus, type },
+            {[field]: value}
+        );
         this.props.setState(state);
     }
 
     dmgTypeIsDisabled(item) {
-        const disabled = this.props.disabledTypes || [];
-        if (this.props.type == item.code) {
+        const {
+            disabledTypes = [], type
+        } = this.props;
+
+        if (type == item.code) {
             return false;
         }
-        if (_.includes(disabled, item.code)) {
+        if (_.includes(disabledTypes, item.code)) {
             return true;
         }
         return false;
     }
 
     render() {
-        return <ControlGroup labels={[this.props.label, "d", "+", "Type"]}>
+        const {
+            label, dice_count, dice_size, bonus = null, type,
+            damage_types = []
+        } = this.props;
+
+        return <ControlGroup labels={[label, "d", "+", "Type"]}>
             <SingleSelect
                 header="Dice count"
-                selected={this.props.dice_count}
+                selected={dice_count}
                 items={this.dice_count}
                 setState={(value) => {
                     this.onFieldChange('dice_count', parseInt(value));
                 }} />
             <SingleSelect
                 header="Dice size"
-                selected={this.props.dice_size}
+                selected={dice_size}
                 items={this.dice_size}
                 setState={(value) => {
                     this.onFieldChange('dice_size', parseInt(value));
                 }} />
             <InputField
                 placeholder="Bonus..."
-                value={this.props.bonus || null}
+                value={bonus || ''}
                 setState={(value) => {
                     this.onFieldChange(
                         'bonus',
@@ -79,8 +81,8 @@ export class DamageEdit extends LazyComponent
                 />
             <SingleSelect
                 header="Damage type"
-                selected={this.props.type}
-                items={this.props.damage_types || []}
+                selected={type}
+                items={damage_types}
                 setState={(value) => {
                     this.onFieldChange('type', value)
                 }}
