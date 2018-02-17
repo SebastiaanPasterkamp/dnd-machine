2c4a8180568c08570e88769537740d1c8314195e
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-02-17 10:50:07 +0100

    ReactJS: Cleaned up Panel code

--- ui/src/jsx/components/Panel.jsx
+++ ui/src/jsx/components/Panel.jsx
@@ -3,47 +3,61 @@ import _ from 'lodash';
 
 import '../../sass/_panel.scss';
 
+import utils from '../utils.jsx';
+
 import LazyComponent from '../components/LazyComponent.jsx';
 
 export class Panel extends LazyComponent
 {
     render() {
-        let style = _.filter([
+        const {
+            className, children, id, header, footer
+        } = this.props;
+
+        let style = utils.makeStyle({}, [
             'nice-panel',
-            this.props.className,
-            ]);
+            className,
+        ]);
         let ContentComponent = 'div';
-        let contentStyle = ["nice-panel-content"];
-        React.Children.map(this.props.children, (child) => {
-            if (
-                child
-                && _.includes(
-                    ['tr', 'tbody', 'thead', 'tfoot'],
-                    child.type
-                )
-            ) {
+        let contentStyle = utils.makeStyle([
+            "nice-panel-content"
+        ]);
+
+        React.Children.map(children, (child) => {
+            if (!child) {
+                return;
+            }
+            if ( _.includes(
+                ['tr', 'tbody', 'thead', 'tfoot'],
+                child.type
+            )) {
                 ContentComponent = 'table';
             }
         });
         if (ContentComponent == 'table') {
-            contentStyle = contentStyle.concat([
+            contentStyle = utils.makeStyle([
+                contentStyle,
                 "nice-table", "bordered"
             ]);
         }
 
-        return <div className={style.join(' ')} id={this.props.id}>
-            {this.props.header
+        console.log(children);
+
+        return <div className={style} id={id}>
+            {header
                 ? <div className="nice-panel-heading">
-                    {this.props.header}
-                </div> : null
+                    {header}
+                </div>
+                : null
             }
-            <ContentComponent className={contentStyle.join(' ')}>
-                {this.props.children}
+            <ContentComponent className={contentStyle}>
+                {children}
             </ContentComponent>
-            {this.props.footer
+            {footer
                 ? <div className="nice-panel-heading">
-                    {this.props.header}
-                </div> : null
+                    {header}
+                </div>
+                : null
             }
         </div>;
     }
