0c1c009fd4132f4ecfd4a7acaa40ba1650dcb1ba
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-03-13 22:52:37 +0100

    App: Initial View+Model of AdventureLeagueLog

---
+++ app/schema/0.0.4.adventureleaguelog.sql
@@ -0,0 +1,9 @@
+-- Create 'adventureleaguelog' table
+
+DROP TABLE IF EXISTS `adventureleaguelog`;
+CREATE TABLE `adventureleaguelog` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `user_id` INTEGER NOT NULL,
+  `character_id` INTEGER NOT NULL,
+  `config` TEXT
+);
