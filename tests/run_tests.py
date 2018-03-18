bb9d1832e0dd737e2ee3c525ddddc9569822da6a
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-03-18 17:47:48 +0100

    App: Added test coverage report

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 import unittest
+import coverage
 
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), 'app'
@@ -12,6 +13,13 @@ sys.path.append(os.path.abspath(os.path.join(
 
 loader = unittest.TestLoader()
 suite = loader.discover('tests')
+cov = coverage.coverage(branch=True, source=['app'])
 
-runner = unittest.TextTestRunner()
-runner.run(suite)
+runner = unittest.TextTestRunner(verbosity=2)
+cov.start()
+result = runner.run(suite)
+if result.wasSuccessful():
+    cov.stop()
+    print('Coverage Summary:')
+    cov.report(show_missing=True)
+    cov.erase()
