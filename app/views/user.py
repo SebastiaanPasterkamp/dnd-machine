7db68801e89c0609405ccc8e5894d2d9797d6cb2
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2018-03-22 23:35:26 +0100

    App: Fixed access restrictions for /user

--- app/views/user.py
+++ app/views/user.py
@@ -9,18 +9,48 @@ class UserBlueprint(BaseApiBlueprint):
         return self.basemapper.user
 
     def _exposeAttributes(self, obj):
-        fields = ['id', 'name', 'role', 'dci']
-
+        exposed = set(['id', 'name', 'dci'])
         if obj.id == request.user.id \
                 or self.checkRole(['admin']):
-            fields.extend(['username', 'email', 'role'])
+            exposed |= set(['username', 'email', 'role'])
+        retval = dict([
+            (key, value)
+            for key, value in obj.config.items()
+            if key in exposed
+            ])
+        return retval
 
-        result = dict([
-            (key, obj[key])
-            for key in fields
+    def _mutableAttributes(self, config, obj=None):
+        mutable = set()
+        if self.checkRole(['admin']):
+            mutable |= set([
+                'username', 'password', 'role', 'email',
+                'name', 'dci',
+                ])
+        if obj is not None and obj.id == request.user.id:
+            mutable |= set([
+                'password', 'email', 'name', 'dci',
+                ])
+        return dict([
+            (key, value)
+            for key, value in config.items()
+            if key in mutable
             ])
 
-        return result
+    def overview(self, *args, **kwargs):
+        if not self.checkRole(['admin']):
+            abort(403)
+        return super(UserBlueprint, self).overview(*args, **kwargs)
+
+    def new(self, *args, **kwargs):
+        if not self.checkRole(['admin']):
+            abort(403)
+        return super(UserBlueprint, self).new(*args, **kwargs)
+
+    def _raw_filter(self, obj):
+        if not self.checkRole(['admin']):
+            abort(403)
+        return obj
 
     def _api_list_filter(self, objs):
         if not self.checkRole(['admin']):
@@ -68,8 +98,7 @@ class UserBlueprint(BaseApiBlueprint):
             )
 
     def new(self):
-        if obj_id != request.user.id \
-                and not self.checkRole(['admin']):
+        if not self.checkRole(['admin']):
             abort(403)
         return render_template(
             'reactjs-layout.html'
