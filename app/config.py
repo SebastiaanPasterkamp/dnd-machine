e923174e1afade6ef70bfb3dd656f4a540ed0214
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-04-02 19:25:55 +0200

    App: Cache json config files once loaded

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ import json
 import codecs
 
 config = None
+data = {}
 def get_config():
     global config
     if config is None:
@@ -11,23 +12,31 @@ def get_config():
             config = json.load(cfg)
     return config
 
-def get_character_data(withMarkdown=False):
+def get_character_data():
+    global data
+    if 'character_data' in data:
+        return data['character-data']
     with open(os.path.join('app', 'character-data.json')) as cfg:
-        character_data = json.load(cfg)
-
-    if withMarkdown:
-        for section in ['race', 'class', 'background']:
-            for part in character_data[section]:
-                mdfile = os.path.join('app', 'data', part['filename'])
-                with codecs.open(mdfile, encoding='utf-8') as fh:
-                    part['description'] = fh.read()
-
-    return character_data
+        data['character-data'] = json.load(cfg)
+    for section in ['race', 'class', 'background']:
+        for part in data['character-data'][section]:
+            mdfile = os.path.join('app', 'data', part['filename'])
+            with codecs.open(mdfile, encoding='utf-8') as fh:
+                part['description'] = fh.read()
+    return data['character-data']
 
 def get_npc_data():
+    global data
+    if 'npc-data' in data:
+        return data['npc-data']
     with open(os.path.join('app', 'npc-data.json')) as cfg:
-        return json.load(cfg)
+        data['npc-data'] = json.load(cfg)
+    return data['npc-data']
 
 def get_item_data():
+    global data
+    if 'item-data' in data:
+        return data['item-data']
     with open(os.path.join('app', 'item-data.json')) as cfg:
-        return json.load(cfg)
+        data['item-data'] = json.load(cfg)
+    return data['item-data']
