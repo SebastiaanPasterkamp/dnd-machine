c1caaf2aa027a3c94fb267d61c1e2bc9316d7b15
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-05-23 20:58:01 +0200

    UI: BaseLinkGroup fixes
    - split buttons off into BaseLinkButton component
    - add PropTypes
    - simplify button filtering

--- ui/src/jsx/components/BaseLinkGroup.jsx
+++ ui/src/jsx/components/BaseLinkGroup.jsx
@@ -9,6 +9,63 @@ import utils from '../utils.jsx';
 
 import LazyComponent from '../components/LazyComponent.jsx';
 
+class BaseLinkButton extends LazyComponent
+{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {
+            altStyle, icon, className, action, download, label, link,
+        } = this.props;
+
+        const style = utils.makeStyle({
+            'nice-btn-alt': altStyle,
+            'nice-btn': !altStyle,
+            'icon': icon,
+            ['fa-' + icon]: icon,
+            [className]: className,
+        }, ['cursor-pointer']);
+
+        if (action) {
+            return <a
+                onClick={action}
+                className={style}
+                >
+                {label}
+            </a>;
+        }
+        if (download) {
+            return <a
+                href={download}
+                className={style}
+                >
+                {label}
+            </a>;
+        }
+        if (link) {
+            return <Link
+                to={link}
+                className={style}
+                >
+                {label}
+            </Link>;
+        }
+        return null;
+    }
+};
+
+BaseLinkButton.propTypes = {
+    altStyle: PropTypes.bool,
+    icon: PropTypes.string,
+    className: PropTypes.string,
+    action: PropTypes.func,
+    download: PropTypes.string,
+    label: PropTypes.string,
+    link: PropTypes.string,
+};
+
 class BaseLinkGroup extends LazyComponent
 {
     constructor(props) {
@@ -23,95 +80,73 @@ class BaseLinkGroup extends LazyComponent
         return [];
     }
 
-    renderButton(button, props) {
-        const {
-            altStyle, icon, className, action, download, label, link
-        } = _.assign({}, this.props, props);
-        const style = utils.makeStyle({
-            'nice-btn-alt': altStyle,
-            'nice-btn': !altStyle,
-            'icon': icon,
-            ['fa-' + icon]: icon,
-            [className]: className,
-        }, ['cursor-pointer']);
-
-        if (action) {
-            return <a
-                key={button}
-                onClick={action}
-                className={style}
-                >
-                {label}
-            </a>;
-        }
-        if (download) {
-            return <a
-                key={button}
-                href={download}
-                className={style}
-                >
-                {label}
-            </a>;
-        }
-        return <Link
-            key={button}
-            to={link}
-            className={style}
-            >
-            {label}
-        </Link>;
-    }
-
     render() {
         const {
-            buttons, className, extra
+            buttons, className, altStyle, extra = [],
         } = this.props;
         const style = utils.makeStyle({
             [className]: className,
         }, ['base-link-group', 'nice-btn-group']);
         const allowed = this.getAllowed();
 
-        const filtered = _.reduce(
-            this.buttonList(),
-            (filtered, func, button) => {
-                const props = func();
-                if (
-                    !_.isNil(buttons)
-                    && !_.includes(buttons, button)
-                ) {
-                    return filtered;
-                }
-                if ('available' in props) {
-                    if (!props.available) {
-                        return filtered;
-                    }
-                } else {
-                    if (!_.includes(allowed, button)) {
-                        return filtered;
-                    }
-                }
-                filtered.push(props);
-                return filtered;
-            },
-            []
-        );
+        const filtered = _(
+                this.buttonList()
+            ).pickBy(
+                (func, action) => (
+                    !buttons || _.includes(buttons, action)
+                )
+            ).mapValues(
+                func => func()
+            ).pickBy(
+                (props, action) => 'available' in props
+                    ? props.available
+                    : _.includes(allowed, action)
+            ).value();
 
         return <div className={style}>
             {_.map(
                 filtered,
-                (props, button) => this.renderButton(button, props)
+                (props, button) => (
+                    <BaseLinkButton
+                        key={ button }
+                        altStyle={ altStyle }
+                        className={ className }
+                        {...props}
+                        />
+                )
             )}
             {_.map(
                 extra,
-                (props, button) => this.renderButton(button, props)
+                (props, button) => (
+                    <BaseLinkButton
+                        key={ button }
+                        altStyle={ altStyle }
+                        className={ className }
+                        {...props}
+                        />
+                )
             )}
         </div>;
     }
 }
 
-BaseLinkGroup.defaultProps = {
-    altStyle: false,
-    extra: [],
+BaseLinkGroup.propTypes = {
+    buttons: PropTypes.arrayOf(
+        PropTypes.string
+    ),
+    className: PropTypes.string,
+    extra: PropTypes.arrayOf(
+        PropTypes.shape({
+            altStyle: PropTypes.bool,
+            icon: PropTypes.string,
+            className: PropTypes.string,
+            action: PropTypes.func,
+            download: PropTypes.string,
+            label: PropTypes.string,
+            link: PropTypes.string,
+        })
+    ),
+    altStyle: PropTypes.bool,
 };
 
 export default BaseLinkGroup;
