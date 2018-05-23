47f242e35962aeef27789a08de02e63532adda0c
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-05-23 23:17:42 +0200

    UI: Tweaked CharacterLinks config

--- ui/src/jsx/components/CharacterLinks.jsx
+++ ui/src/jsx/components/CharacterLinks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import { userHasRole } from '../utils.jsx';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import ObjectDataActions from '../actions/ObjectDataActions.jsx';
 
@@ -8,7 +8,9 @@ import BaseLinkGroup from '../components/BaseLinkGroup.jsx';
 import ListDataWrapper from '../hocs/ListDataWrapper.jsx';
 import ObjectDataWrapper from '../hocs/ObjectDataWrapper.jsx';
 
-class CharacterLinks extends BaseLinkGroup
+import { userHasRole } from '../utils.jsx';
+
+export class CharacterLinks extends BaseLinkGroup
 {
     constructor(props) {
         super(props);
@@ -16,8 +18,12 @@ class CharacterLinks extends BaseLinkGroup
 
     buttonList() {
         const {
-            character, character_id, current_user: user
+            character = {}, character_id, current_user: user
         } = this.props;
+
+        if (!user) {
+            return {};
+        }
 
         const levelUp = (
             character
@@ -25,53 +31,64 @@ class CharacterLinks extends BaseLinkGroup
             && character.level_up.config.length
         );
 
-        if (!user) {
-            return {};
-        }
-
         return {
             'view': () => ({
                 label: 'View',
                 link: "/character/show/" + character_id,
                 icon: 'eye',
-                available: character && userHasRole(user, 'player'),
+                available: (
+                    character_id
+                    && userHasRole(user, ['player', 'dm'])
+                ),
             }),
             'raw': () => ({
                 label: 'Raw',
                 link: "/character/raw/" + character_id,
                 icon: 'cogs',
-                available: character && userHasRole(user, 'admin'),
+                available: (
+                    character_id
+                    && userHasRole(user, 'admin')
+                ),
             }),
             'edit': () => ({
-                label: 'Edit',
+                label: levelUp ? 'Level Up' : 'Edit',
                 link: "/character/edit/" + character_id,
                 icon: levelUp ? 'level-up' : 'pencil',
-                color: levelUp ? 'primary' : null,
-                available: character && (
-                    character.user_id == user.id
-                    || userHasRole(user, ['admin', 'dm'])
+                className: levelUp ? 'primary' : null,
+                available: (
+                    character_id
+                    && character.user_id == user.id
                 ),
             }),
             'copy': () => ({
                 label: 'Copy',
                 link: "/character/copy/" + character_id,
                 icon: 'clone',
-                available: character && (
-                    character.user_id == user.id
-                    || userHasRole(user, ['admin', 'dm'])
+                available: (
+                    character_id
+                    && (
+                        character.user_id == user.id
+                        || userHasRole(user, ['dm'])
+                    )
                 ),
             }),
-            'download': () => ({
+            'pdf': () => ({
                 label: 'PDF',
                 download: "/character/download/" + character_id,
                 icon: 'file-pdf-o',
-                available: character && userHasRole(user, ['player', 'dm']),
+                available: (
+                    character_id
+                    && userHasRole(user, ['player', 'dm'])
+                ),
             }),
             'new': () => ({
                 label: 'New',
                 link: "/character/new",
                 icon: 'plus',
-                available: !character && userHasRole(user, ['player', 'dm']),
+                available: (
+                    !character_id
+                    && userHasRole(user, ['player', 'dm'])
+                ),
             }),
             'delete': () => ({
                 label: 'Delete',
@@ -81,16 +98,36 @@ class CharacterLinks extends BaseLinkGroup
                     );
                 },
                 icon: 'trash-o',
-                color: 'bad',
-                available: character && character.user_id == user.id,
+                className: 'bad',
+                available: (
+                    character_id
+                    && character.user_id == user.id
+                ),
             }),
         };
     }
 }
 
-CharacterLinks.defaultProps = {
-    buttons: ['view', 'edit', 'copy', 'pdf'],
-};
+CharacterLinks.propTypes = _.assign(
+    {}, BaseLinkGroup.propTypes, {
+        buttons: PropTypes.arrayOf(
+            PropTypes.oneOf([
+                'view', 'edit', 'copy', 'raw', 'new', 'pdf', 'delete',
+            ])
+        ),
+        character_id: PropTypes.number,
+        current_user: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            role: PropTypes.arrayOf(
+                PropTypes.oneOf(['player', 'dm', 'admin'])
+            ),
+        }),
+        character: PropTypes.shape({
+            user_id: PropTypes.number.isRequired,
+            level_up: PropTypes.object,
+        }),
+    }
+);
 
 export default ListDataWrapper(
     ObjectDataWrapper(CharacterLinks, [
