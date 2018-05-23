650de42d0863da82637223b9d72dfe35f760146c
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-05-23 23:27:48 +0200

    UI Test: Add EncounterLinks snapshots

---
+++ ui/tests/components/EncounterLinks.react-test.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { EncounterLinks } from 'components/EncounterLinks.jsx';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Component: EncounterLinks', () => {
+    it('should render without props', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <EncounterLinks
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render a new button', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <EncounterLinks
+                    current_user={{
+                        id: 1,
+                        role: ['admin'],
+                    }}
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should not show buttons for players', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <EncounterLinks
+                    encounter_id={10}
+                    current_user={{
+                        id: 1,
+                        role: ['player'],
+                    }}
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should show a view and edit button', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <EncounterLinks
+                    encounter_id={10}
+                    current_user={{
+                        id: 1,
+                        role: ['dm'],
+                    }}
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
