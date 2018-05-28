cdbb2605fb48fb9a38cf96041193bf83bd148dde
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-05-28 23:32:12 +0200

    UI Tests: Add Action coverage for PartyLinks

--- ui/tests/components/PartyLinks.react-test.jsx
+++ ui/tests/components/PartyLinks.react-test.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import { PartyLinks } from 'components/PartyLinks.jsx';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Component: PartyLinks', () => {
+    beforeEach(() => {
+        fetch.resetMocks()
+    })
+
     it('should render without props', () => {
         const tree = renderer.create(
             <MemoryRouter>
@@ -134,5 +139,88 @@ describe('Component: PartyLinks', () => {
         ).toJSON();
 
         expect(tree).toMatchSnapshot();
+    });
+
+    it('hosting should work', () => {
+
+        fetch.mockResponseOnce(JSON.stringify({
+            id: 10,
+            user_id: 1,
+            name: 'Host me',
+        }));
+
+        const wrapper = mount(
+            <MemoryRouter>
+                <PartyLinks
+                    party_id={10}
+                    party={{
+                        id: 10,
+                        user_id: 1,
+                    }}
+                    current_user={{
+                        id: 2,
+                        role: ['dm'],
+                    }}
+                    />
+            </MemoryRouter>
+        );
+
+        wrapper
+            .find('a.fa-beer')
+            .simulate('click');
+
+        expect(fetch).toBeCalledWith(
+            '/party/host/10',
+            {
+                credentials: 'same-origin',
+                method: 'POST',
+                'headers': {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            }
+        );
+    });
+
+    it('stop hosting should work', () => {
+
+        fetch.mockResponseOnce(JSON.stringify({
+            id: 10,
+            user_id: 1,
+            name: 'Host me',
+        }));
+
+        const wrapper = mount(
+            <MemoryRouter>
+                <PartyLinks
+                    party_id={10}
+                    party={{
+                        id: 10,
+                        user_id: 1,
+                    }}
+                    hosted_party={{
+                        id: 10,
+                    }}
+                    current_user={{
+                        id: 2,
+                        role: ['dm'],
+                    }}
+                    />
+            </MemoryRouter>
+        );
+
+        wrapper
+            .find('a.fa-ban')
+            .simulate('click');
+
+        expect(fetch).toBeCalledWith(
+            '/party/host',
+            {
+                credentials: 'same-origin',
+                method: 'POST',
+                'headers': {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            }
+        );
     });
 });
