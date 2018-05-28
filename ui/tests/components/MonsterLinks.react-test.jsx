753e18b92b1a9a85cea765087c211efe66c4b86a
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-05-28 23:27:06 +0200

    UI Tests: Add Action coverage for MonsterLinks

--- ui/tests/components/MonsterLinks.react-test.jsx
+++ ui/tests/components/MonsterLinks.react-test.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import { MonsterLinks } from 'components/MonsterLinks.jsx';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Component: MonsterLinks', () => {
+    beforeEach(() => {
+        fetch.resetMocks()
+    })
+
     it('should render without props', () => {
         const tree = renderer.create(
             <MemoryRouter>
@@ -76,5 +81,40 @@ describe('Component: MonsterLinks', () => {
         ).toJSON();
 
         expect(tree).toMatchSnapshot();
+    });
+
+    it('copy should work submit', () => {
+
+        fetch.mockResponseOnce(JSON.stringify({
+            id: 11,
+            name: 'Example (copy)',
+        }));
+
+        const wrapper = mount(
+            <MemoryRouter>
+                <MonsterLinks
+                    monster_id={10}
+                    current_user={{
+                        id: 1,
+                        role: ['dm'],
+                    }}
+                    />
+            </MemoryRouter>
+        );
+
+        wrapper
+            .find('a.fa-clone')
+            .simulate('click');
+
+        expect(fetch).toBeCalledWith(
+            '/monster/copy/10',
+            {
+                credentials: 'same-origin',
+                method: 'GET',
+                'headers': {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            }
+        );
     });
 });
