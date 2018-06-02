663b711b5bd011defb4621c6978200a91a81e408
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-06-02 22:49:57 +0200

    UI: Fix ChallengeRating w/ 0 CR

--- ui/src/jsx/components/ChallengeRating.jsx
+++ ui/src/jsx/components/ChallengeRating.jsx
@@ -12,25 +12,33 @@ class ChallengeRating extends LazyComponent
         const {
             challengeRating, precise = false
         } = this.props;
+
         if (_.isNil(challengeRating)) {
             return <div className="challenge-rating inline">
                 CR&nbsp;&mdash;
             </div>;
         }
 
-        let value = challengeRating.toFixed(3);
-        if (!precise) {
-            value = utils.closest({
-                0: 0.0,
-                '\u215B': 0.125,
-                '\u00BC': 0.25,
-                '\u00BD': 0.5,
-                [Math.round(value)]: 1.0,
-            }, value, value);
+        if (precise) {
+            return <div className="challenge-rating inline">
+                CR&nbsp;{challengeRating.toFixed(3)}
+            </div>;
         }
 
+        const value = utils.closest({
+            '0': 0.0,
+            '\u215B': 0.125,
+            '\u00BC': 0.25,
+            '\u00BD': 0.5,
+            'round': 1.0,
+        }, challengeRating, challengeRating);
+
         return <div className="challenge-rating inline">
-            CR&nbsp;{value}
+            CR&nbsp;{
+                value == 'round'
+                ? Math.round(challengeRating)
+                : value
+            }
         </div>;
     }
 }
