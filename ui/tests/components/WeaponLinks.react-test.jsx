3c4f623ed2049cfa6e92c8d1f8350f23043c77d3
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-06-18 23:20:57 +0200

    UI Test: Coverage for WeaponLinks

---
+++ ui/tests/components/WeaponLinks.react-test.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { WeaponLinks } from 'components/WeaponLinks.jsx';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Component: WeaponLinks', () => {
+    it('should render without props', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <WeaponLinks
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should render a new button', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <WeaponLinks
+                    current_user={{
+                        id: 1,
+                        role: ['admin'],
+                    }}
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should show only a view button', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <WeaponLinks
+                    weapon_id={10}
+                    current_user={{
+                        id: 1,
+                        role: ['player'],
+                    }}
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should show a view and edit button', () => {
+        const tree = renderer.create(
+            <MemoryRouter>
+                <WeaponLinks
+                    weapon_id={10}
+                    current_user={{
+                        id: 1,
+                        role: ['dm'],
+                    }}
+                    />
+            </MemoryRouter>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
