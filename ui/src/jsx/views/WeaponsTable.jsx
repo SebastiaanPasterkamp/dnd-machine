97abfddc0df08d53b1cee34b3c8e038e11dd9154
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-06-19 21:42:07 +0200

    UI: Weapon Table search form

--- ui/src/jsx/views/WeaponsTable.jsx
+++ ui/src/jsx/views/WeaponsTable.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import '../../sass/_weapons-table.scss';
+
 import ListDataWrapper from '../hocs/ListDataWrapper.jsx';
 import ObjectDataListWrapper from '../hocs/ObjectDataListWrapper.jsx';
 
 import LazyComponent from '../components/LazyComponent.jsx';
+import ControlGroup from '../components/ControlGroup.jsx';
 import Coinage from '../components/Coinage.jsx';
 import DiceNotation from '../components/DiceNotation.jsx';
+import InputField from '../components/InputField.jsx';
 import ListLabel from '../components/ListLabel.jsx';
+import MultiSelect from '../components/MultiSelect.jsx';
 import Reach from '../components/Reach.jsx';
 import WeaponLinks from '../components/WeaponLinks.jsx';
 
@@ -50,7 +55,7 @@ class WeaponsRow extends LazyComponent
     render() {
         const {
             id, name, type, damage, range, cost, property,
-            weapon_properties = [], weapon_types = []
+            weapon_properties = [], weapon_types = [],
         } = this.props;
 
         return <tr data-id={id}>
@@ -94,29 +99,169 @@ class WeaponsRow extends LazyComponent
 
 class WeaponsTable extends LazyComponent
 {
-    shouldDisplayRow(pattern, weapon) {
-        return (
-            (weapon.name && weapon.name.match(pattern))
+    constructor(props) {
+        super(props);
+
+        this.dice_size = _.map(
+            [4, 6, 8, 10, 12],
+            size => ({
+                code: size,
+                label: size
+            })
         );
+
+        this.state = {
+            name: '',
+            dice_count: '',
+            dice_size: [],
+            damage_type: [],
+            weapon_type: [],
+            weapon_property: [],
+        };
+    }
+
+    shouldDisplayRow(row, filter={}) {
+        return (
+            row.name.match(filter.pattern)
+            && (
+                !filter.weapon_type.length
+                || _.includes(filter.weapon_type, row.type)
+            )
+            && (
+                !filter.dice_count
+                || filter.dice_count == row.damage.dice_count
+            )
+            && (
+                !filter.dice_size.length
+                || _.includes(filter.dice_size, row.damage.dice_size)
+            )
+            && (
+                !filter.damage_type.length
+                || _.includes(filter.damage_type, row.damage.type)
+            )
+            && _.intersection(
+                filter.weapon_property,
+                row.property
+            ).length == filter.weapon_property.length
+        );
+    }
+
+    renderFilters() {
+        const {
+            weapon_types = [],
+            damage_types = [],
+            weapon_properties = [],
+        } = this.props;
+        const {
+            name,
+            dice_count,
+            dice_size,
+            damage_type,
+            weapon_type,
+            weapon_property,
+        } = this.state;
+
+        return <div className="weapons-table--search">
+            <ControlGroup label="Name">
+                <InputField
+                    id="name"
+                    type="text"
+                    value={name}
+                    placeholder="Name..."
+                    setState={
+                        name => this.setState({name})
+                    } />
+            </ControlGroup>
+            <ControlGroup label="Properties">
+                <MultiSelect
+                    id="properties"
+                    items={weapon_properties}
+                    selected={weapon_property}
+                    emptyLabel="All Properties"
+                    setState={
+                        weapon_property => this.setState({
+                            weapon_property
+                        })
+                    } />
+            </ControlGroup>
+            <ControlGroup label="Weapon Types">
+                <MultiSelect
+                    id="weapon-types"
+                    items={weapon_types}
+                    selected={weapon_type}
+                    emptyLabel="All Weapon Types"
+                    setState={
+                        weapon_type => this.setState({
+                            weapon_type
+                        })
+                    } />
+            </ControlGroup>
+            <ControlGroup labels={["Damage", "d", "Type"]}>
+                <InputField
+                    type="number"
+                    placeholder="Dice count"
+                    value={ dice_count || '' }
+                    setState={
+                        dice_count => this.setState({dice_count})
+                    }
+                    />
+                <MultiSelect
+                    items={ this.dice_size }
+                    selected={ dice_size }
+                    emptyLabel="Dice Sizes"
+                    setState={
+                        dice_size => this.setState({
+                            dice_size
+                        })
+                    } />
+                <MultiSelect
+                    id="damage-types"
+                    items={ damage_types }
+                    selected={ damage_type }
+                    emptyLabel="All Damage Types"
+                    setState={
+                        damage_type => this.setState({
+                            damage_type
+                        })
+                    } />
+            </ControlGroup>
+        </div>;
     }
 
     render() {
         const {
-            weapons, search = '', weapon_properties, weapon_types
+            weapons = [], search = '',
+            weapon_properties = [], weapon_types = [],
         } = this.props;
+        const {
+            name,
+            dice_count,
+            dice_size,
+            damage_type,
+            weapon_type,
+            weapon_property,
+        } = this.state;
 
         if (!weapons) {
             return null;
         }
 
-        let pattern = new RegExp(search, "i");
+        const filter = {
+            pattern: new RegExp(name || search || '', "i"),
+            dice_count,
+            dice_size,
+            damage_type,
+            weapon_type,
+            weapon_property,
+        }
         const filtered = _.filter(
             weapons,
-            (weapon) => this.shouldDisplayRow(pattern, weapon)
+            weapon => this.shouldDisplayRow(weapon, filter)
         );
 
-        return <div>
+        return <div className="weapons-table">
             <h2 className="icon fa-cutlery">Weapons</h2>
+            {this.renderFilters()}
             <table className="nice-table condensed bordered responsive">
                 <WeaponsHeader />
                 <tbody key="tbody">
@@ -140,6 +285,11 @@ export default ListDataWrapper(
         WeaponsTable,
         {weapons: {group: 'items', type: 'weapon'}}
     ),
-    ['search', 'weapon_properties', 'weapon_types'],
+    [
+        'search',
+        'damage_types',
+        'weapon_properties',
+        'weapon_types',
+    ],
     'items'
 );
