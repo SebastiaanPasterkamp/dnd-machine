2fe577a2521532468ac95baf6ae00efa0f2e068c
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-06-20 22:13:06 +0200

    UI: Update DefinitionList style / logic

--- ui/src/jsx/components/DefinitionList.jsx
+++ ui/src/jsx/components/DefinitionList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 import '../../sass/_list-component.scss';
@@ -10,69 +11,75 @@ import MarkdownTextField from './MarkdownTextField.jsx';
 
 export class Definition extends LazyComponent
 {
-    onFieldChange(field, value, callback=null) {
-        this.props.setState({
-            [field]: value
-        }, callback);
-    }
-
     render() {
         const {
-            name, description = ''
+            name, description = '',
+            setState,
         } = this.props;
 
         return <div>
             <InputField
                 placeholder="Name..."
-                value={name}
-                setState={(value) => {
-                    this.onFieldChange('name', value);
-                }}
+                value={ name }
+                setState={ name => setState({ name }) }
                 />
             <MarkdownTextField
                 placeholder="Description..."
-                value={description}
+                value={ description }
                 rows={5}
-                setState={(value) => {
-                    this.onFieldChange('description', value);
-                }} />
+                setState={ description => setState({ description }) }
+                />
         </div>
     }
 }
+
+Definition.propTypes = {
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    setState: PropTypes.func.isRequired,
+};
 
 export class DefinitionList extends LazyComponent
 {
     onSetState(value, callback) {
         const {
-            list, setState
+            list, setState,
         } = this.props;
 
-        setState(_.reduce(
-            value,
-            (list, {name, description}) => {
-                list[name] = description;
-                return list;
-            },
-            {}
-        ), callback);
+        if (!setState) {
+            return;
+        }
+
+        setState(
+            _.fromPairs(
+                _.map(
+                    value,
+                    ({name, description}) => ([name, description])
+                )
+            ),
+            callback
+        );
     }
 
     render() {
         const {
-            list, setState, ...rest
+            list, setState, ...props
         } = this.props;
 
         return <ListComponent
-            {...rest}
-            list={_.map(list, (description, name) => ({
-                name, description
-            }))}
-            component={Definition}
-            initialItem={{name: '', description: ''}}
+            newItem="auto"
+            {...props}
+            list={ _.map(
+                list,
+                (description, name) => ({ name, description })
+            ) }
+            component={ Definition }
+            initialItem={{ name: '', description: '' }}
             keyProp="name"
-            setState={setState
-                ? (value, callback=null) => this.onSetState(value, callback)
-                : null
+            setState={
+                (value, callback=null) => this.onSetState(
+                    value, callback
+                )
             }
             />
     }
@@ -82,6 +89,11 @@ DefinitionList.defaultProps = {
     setState: (value) => {
         console.log(['DefinitionList', value]);
     }
+};
+
+DefinitionList.propTypes = {
+    list: PropTypes.object.isRequired,
+    setState: PropTypes.func.isRequired,
 };
 
 export default DefinitionList;
