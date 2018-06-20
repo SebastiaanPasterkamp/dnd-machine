45a86fcb3457e026de3e16b824da9f3f7d63a346
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-06-20 22:45:11 +0200

    UI: PropTyes for ModalDialog

--- ui/src/jsx/components/ModalDialog.jsx
+++ ui/src/jsx/components/ModalDialog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import '../../sass/_modal-dialog.scss';
 
@@ -13,23 +14,24 @@ class ModalDialogLinks extends BaseLinkGroup
     }
 
     buttonList() {
-        const { onCancel, onSave, onDone } = this.props;
+        const {
+            onDone,
+            doneLabel = 'Done',
+            onCancel,
+            cancelLabel = 'Cancel',
+        } = this.props;
 
         return {
             'cancel': () => ({
-                label: 'Cancel',
-                action: () => onCancel(),
+                label: cancelLabel,
+                icon: 'fa-cross',
+                action: onCancel,
                 available: !!onCancel,
             }),
-            'save': () => ({
-                label: 'Save',
-                action: () => onSave(),
-                className: 'primary',
-                available: !!onSave,
-            }),
             'done': () => ({
-                label: 'Done',
-                action: () => onDone(),
+                label: doneLabel,
+                icon: 'fa-check',
+                action: onDone,
                 className: 'primary',
                 available: !!onDone,
             })
@@ -37,12 +39,18 @@ class ModalDialogLinks extends BaseLinkGroup
     }
 }
 
+ModalDialogLinks.propTypes = {
+    onCancel: PropTypes.func,
+    onSave: PropTypes.func,
+    onDone: PropTypes.func,
+};
+
 export class ModalDialog extends LazyComponent
 {
     render() {
         const {
-            onCancel, onClose, label, subheading, children,
-            onHelp, ...props
+            onCancel, label, subheading, children,
+            onHelp, helpLabel = 'Help', ...props
         } = this.props;
 
         return <div
@@ -52,44 +60,49 @@ export class ModalDialog extends LazyComponent
                 <div className="nice-modal-header">
                     <a
                         className="nice-modal-close"
-                        onClick={() => {
-                            onClose
-                                ? onClose()
-                                : onCancel()
-                        }}
+                        onClick={ onCancel }
                         >
                         <i className="icon fa-times"></i>
                     </a>
-                    <h4>{label}</h4>
+                    <h4>{ label }</h4>
                 </div>
-                {subheading
-                    ? <div className="nice-modal-sub">
-                        {subheading}
-                    </div> : null
-                }
+
+                {subheading && <div className="nice-modal-sub">
+                    { subheading }
+                </div>  }
+
                 <div className="nice-modal-body overflow-x-scroll">
-                    {children}
+                    { children }
                 </div>
+
                 <div className="nice-modal-footer">
-                    {onHelp
-                        ? <a
-                            className="nice-btn link icon fa-question"
-                            onClick={() => onHelp()}
-                            >
-                            Help
-                        </a>
-                        : null
-                    }
+                    {onHelp && <a
+                        className="nice-btn link icon fa-question"
+                        onClick={ onHelp }
+                        >
+                        { helpLabel }
+                    </a> }
                     <ModalDialogLinks
                         className="pull-right"
                         {...props}
-                        onCancel={onCancel}
-                        onClose={onClose}
+                        onCancel={ onCancel }
                         />
                 </div>
             </div>
         </div>;
     }
 }
+
+ModalDialog.propTypes = {
+    label: PropTypes.string.isRequired,
+    children: PropTypes.any.isRequired,
+    subheading: PropTypes.string,
+    onCancel: PropTypes.func,
+    cancelLabel: PropTypes.string,
+    onDone: PropTypes.func,
+    doneLabel: PropTypes.string,
+    onHelp: PropTypes.func,
+    helpLabel: PropTypes.string,
+};
 
 export default ModalDialog;
