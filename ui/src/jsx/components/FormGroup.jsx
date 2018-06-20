ea2b913b1dcfb96234928cd409b854c87d8b86ea
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2018-06-20 22:44:50 +0200

    UI: Remove dead code from FormGroup

--- ui/src/jsx/components/FormGroup.jsx
+++ ui/src/jsx/components/FormGroup.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import LazyComponent from '../components/LazyComponent.jsx';
 
-import _ from 'lodash';
-
 export class FormGroup extends LazyComponent
 {
-    renderLabel(index, label) {
-        return <span
-                key={"label-" + index}
-                className="nice-input-addon">
-            {label}
-        </span>;
-    }
-
     render() {
+        const {
+            label, children,
+        } = this.props;
         return <div className="nice-form-group">
-            <label>{this.props.label}</label>
-            {this.props.children}
+            <label>{ label }</label>
+            { children }
         </div>;
     }
 }
