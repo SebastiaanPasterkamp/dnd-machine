c79b6a301120d84cc11610d2268899484cb3d544
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-01-02 20:30:55 +0100

    UI: Add propTypes / fixes to UserEdit/UserView

--- ui/src/jsx/views/UserView.jsx
+++ ui/src/jsx/views/UserView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 import '../../sass/_user-view.scss';
@@ -15,9 +16,8 @@ export class UserView extends React.Component
     render() {
         const {
             id, name, username, dci, email, role = [],
-            user_roles = [], current_user
+            user_roles = [],
         } = this.props;
-        const isAdmin = _.includes(current_user.role || [], 'admin');
 
         return <React.Fragment>
             <Panel
@@ -70,22 +70,33 @@ export class UserView extends React.Component
     }
 }
 
+UserView.propTypes = {
+    id: PropTypes.number,
+    name: PropTypes.string,
+    dci: PropTypes.string,
+    email: PropTypes.string,
+    role: PropTypes.arrayOf(
+        PropTypes.string
+    ),
+    user_roles: PropTypes.arrayOf(
+        PropTypes.shape({
+            code: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+        })
+    ),
+};
+
 export default ListDataWrapper(
-    ListDataWrapper(
-        RoutedObjectDataWrapper(
-            UserView, {
-                className: 'user-view',
-                icon: 'fa-user',
-                label: 'User',
-            },
-            "user"
-        ),
-        [
-            "user_roles",
-        ],
-        'items'
+    RoutedObjectDataWrapper(
+        UserView, {
+            className: 'user-view',
+            icon: 'fa-user',
+            label: 'User',
+        },
+        "user"
     ),
     [
-        "current_user",
-    ]
+        "user_roles",
+    ],
+    'items'
 );
