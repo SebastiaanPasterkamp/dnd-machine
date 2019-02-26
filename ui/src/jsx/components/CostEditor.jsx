5121bcce644f4d7c48cb8fc5f0d420468b94b034
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-02-26 22:09:55 +0100

    UI: CostEditor only passes on numeric values

--- ui/src/jsx/components/CostEditor.jsx
+++ ui/src/jsx/components/CostEditor.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 import LazyComponent from '../components/LazyComponent.jsx';
@@ -15,17 +16,63 @@ class CostEditor extends LazyComponent
             {code: 'gp', label: 'Gold'},
             {code: 'pp', label: 'Platinum'}
         ];
+        this.state = {};
+    }
+
+    onSetState = (newValue) => {
+        let value = {}
+        try {
+            value = _.mapValues(
+                newValue,
+                amount => {
+                    const asNumber = parseInt(amount, 10);
+                    if (_.isNaN(asNumber)) {
+                        throw amount;
+                    }
+                    return asNumber;
+                }
+            );
+        } catch(e) {
+            this.setState({ value: newValue });
+            return;
+        }
+
+        this.setState(
+            { value: undefined },
+            this.props.setState(value)
+        );
     }
 
     render() {
-        const { value = {}, ...props } = this.props;
+        const {
+            value: propValue,
+            setState,
+            ...props
+        } = this.props;
+        const {
+            value = propValue
+        } = this.state;
 
         return <TagValueContainer
             {...this.props}
+            value={value}
+            setState={this.onSetState}
             items={this.coinage}
             defaultValue={1}
-            />;
+        />;
     }
 }
+
+CostEditor.propTypes = {
+    value: PropTypes.objectOf(
+        PropTypes.number
+    ).isRequired,
+    setState: PropTypes.func,
+};
+
+CostEditor.defaultProps = {
+    value: {},
+    setState: null,
+};
 
 export default CostEditor;
