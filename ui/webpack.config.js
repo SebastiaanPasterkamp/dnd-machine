824b0c4692ce7142f4d4f4b220c1d83a38d622cf
Sebastiaan Pasterkamp <sebastiaan.pasterkamp@gmail.com>
2019-04-02 21:46:45 +0200

    UI: Update webpack path names

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -4,17 +4,17 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 var PROJECT = 'dnd-machine';
-var BUILD_DIR_JSX = path.resolve(__dirname, '..', 'app', 'static', 'js');
-var APP_DIR_JSX = path.resolve(__dirname, 'src', 'jsx');
-var BUILD_DIR_SASS = 'app/static/css';
-var APP_DIR_SASS = path.resolve(__dirname, 'src', 'sass');
-var BUILD_DIR_IMG = '../img/';
-var APP_DIR_IMG = path.resolve(__dirname, 'src', 'img');
+var OUTPUT_PATH_JSX = path.resolve(__dirname, '..', 'app', 'static', 'js');
+var SOURCE_PATH_JSX = path.resolve(__dirname, 'src', 'jsx');
+var OUTPUT_PATH_SASS = '../css';
+var SOURCE_PATH_SASS = path.resolve(__dirname, 'src', 'sass');
+var OUTPUT_PATH_IMG = '../img/';
+var SOURCE_PATH_IMG = path.resolve(__dirname, 'src', 'img');
 
 var config = {
-    entry: APP_DIR_JSX + '/index.jsx',
+    entry: SOURCE_PATH_JSX + '/index.jsx',
     output: {
-        path: BUILD_DIR_JSX,
+        path: OUTPUT_PATH_JSX,
         filename: PROJECT + '.js'
     },
     resolve: {
@@ -24,7 +24,7 @@ var config = {
         loaders : [
             {
                 test: /\.jsx?/,
-                include: APP_DIR_JSX,
+                include: SOURCE_PATH_JSX,
                 loader: 'babel-loader',
             },
             {
@@ -32,12 +32,12 @@ var config = {
                 loader: 'file-loader',
                 options: {
                     name: '[name].[ext]',
-                    outputPath: BUILD_DIR_IMG,
+                    outputPath: OUTPUT_PATH_IMG,
                 }
             },
             {
                 test: /\.json$/i,
-                include : APP_DIR_JSX,
+                include : SOURCE_PATH_JSX,
                 loader : 'json-loader'
             },
             {
@@ -54,7 +54,7 @@ var config = {
                         options: {
                             sourceMap: true,
                             includePaths: [
-                                APP_DIR_SASS,
+                                SOURCE_PATH_SASS,
                             ],
                         },
                     }
@@ -64,7 +64,7 @@ var config = {
     },
     plugins: [
         new ExtractTextPlugin({
-            filename: '../css/' + PROJECT + '.css',
+            filename: `${OUTPUT_PATH_SASS}/${PROJECT}.css`,
             allChunks: true
         }),
         new ProgressBarPlugin()
