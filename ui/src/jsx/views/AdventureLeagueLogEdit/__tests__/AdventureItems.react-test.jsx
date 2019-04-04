cb5c6ee2098c3205e5a7150b10b77d17994833cb
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-04-05 00:00:04 +0200

    UI Test: Improve coverage for AL Items/Unlocks

--- ui/src/jsx/views/AdventureLeagueLogEdit/__tests__/AdventureItems.react-test.jsx
+++ ui/src/jsx/views/AdventureLeagueLogEdit/__tests__/AdventureItems.react-test.jsx
@@ -8,6 +8,9 @@ describe('AdventureItems', () => {
     const fullProps = {
         label: "Example Items",
         className: "example-items",
+        starting: 0,
+        earned: ["Health Potion"],
+        total: 1,
     };
 
     it('should render with minimum props', () => {
@@ -21,5 +24,86 @@ describe('AdventureItems', () => {
         );
 
         expect(tree).toMatchSnapshot();
+    });
+
+    it('should render with full props', () => {
+        const setState = jest.fn();
+
+        const tree = renderer.create(
+            <AdventureItems
+                setState={setState}
+                {...fullProps}
+            />
+        );
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should add an item', () => {
+        const setState = jest.fn();
+
+        const wrapper = mount(
+            <AdventureItems
+                setState={setState}
+                label={fullProps.label}
+            />
+        );
+
+        wrapper
+            .find('input[disabled=false]')
+            .simulate('change', {target: {
+                value: "Health Potion",
+            }});
+
+        expect(setState).toBeCalledWith({
+            starting: 0,
+            earned: ["Health Potion"],
+            total: 1,
+        });
+    });
+
+    it('should remove an item', () => {
+        const setState = jest.fn();
+
+        const wrapper = mount(
+            <AdventureItems
+                setState={setState}
+                {...fullProps}
+            />
+        );
+
+        wrapper
+            .find('input[disabled=false]')
+            .at(0)
+            .simulate('change', {target: {
+                value: "",
+            }});
+
+        expect(setState).toBeCalledWith({
+            starting: 0,
+            earned: [],
+            total: 0,
+        });
+    });
+
+    it('should remove an empty item', () => {
+        const setState = jest.fn();
+
+        const wrapper = mount(
+            <AdventureItems
+                setState={setState}
+                {...fullProps}
+            />
+        );
+
+        wrapper
+            .find('button[name="del"]')
+            .simulate('click');
+
+        expect(setState).toBeCalledWith({
+            starting: 0,
+            earned: [],
+            total: 0,
+        });
     });
 });
