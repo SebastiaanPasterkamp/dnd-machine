d40d65781fd852e44aead930b10c527dbdbf6893
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-04-16 21:54:51 +0200

    UI: PartyLinks use new BaseLinkGroup

--- ui/src/jsx/components/PartyLinks.jsx
+++ ui/src/jsx/components/PartyLinks.jsx
@@ -1,162 +1,203 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
+import {
+    BaseLinkButton,
+    BaseLinkGroup,
+} from '../components/BaseLinkGroup/index.jsx';
+import ListDataActions from '../actions/ListDataActions.jsx';
+import ListDataWrapper from '../hocs/ListDataWrapper.jsx';
 import ObjectDataActions from '../actions/ObjectDataActions.jsx';
-
+import ObjectDataWrapper from '../hocs/ObjectDataWrapper.jsx';
 import { userHasRole } from '../utils.jsx';
 
-import ListDataActions from '../actions/ListDataActions.jsx';
-
-import BaseLinkGroup from '../components/BaseLinkGroup.jsx';
-import ListDataWrapper from '../hocs/ListDataWrapper.jsx';
-import ObjectDataWrapper from '../hocs/ObjectDataWrapper.jsx';
 
 export class PartyLinks extends BaseLinkGroup
 {
-    constructor(props) {
-        super(props);
+    onHost = () => {
+        const { id } = this.props;
+        fetch(`/party/host/${id}`, {
+            method: "POST",
+            credentials: 'same-origin',
+            'headers': {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            ListDataActions.fetchItems.completed({
+                hosted_party: data
+            });
+        });
     }
 
-    buttonList() {
+    onUnhost = () => {
+        fetch("/party/host", {
+        method: "POST",
+            credentials: 'same-origin',
+            'headers': {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            ListDataActions.fetchItems.completed({
+                hosted_party: data
+            });
+        });
+    }
+
+    onDelete = () => {
+        const { id } = this.props;
+        ObjectDataActions.deleteObject("character", id);
+    }
+
+    render() {
         const {
+            id,
+            hostedParty,
             party: {
-                user_id,
-                member_ids = [],
+                user_id: userId,
             } = {},
-            party_id,
-            hosted_party: hosted,
-            current_user: user,
+            currentUser,
+            altStyle,
+            children,
+            ...props
         } = this.props;
 
-        if (!user) {
-            return {};
+        if (!currentUser) {
+            return null;
         }
 
-        return {
-            'view': () => ({
-                label: 'View',
-                link: "/party/show/" + party_id,
-                icon: 'eye',
-                available: (
-                    party_id !== undefined
-                    && (
-                        user_id === user.id
-                        || userHasRole(user, ['dm', 'admin'])
-                    )
-                ),
-            }),
-            'edit': () => ({
-                label: 'Edit',
-                link: "/party/edit/" + party_id,
-                icon: 'pencil',
-                available: (
-                    party_id !== undefined
-                    && (
-                        user_id === user.id
-                        || userHasRole(user, 'admin')
-                    )
-                ),
-            }),
-            'host': () => (hosted && party_id === hosted.id ? {
-                label: 'Stop',
-                action: () => {
-                    fetch("/party/host", {
-                    method: "POST",
-                        credentials: 'same-origin',
-                        'headers': {
-                            'X-Requested-With': 'XMLHttpRequest'
-                        }
-                    })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        ListDataActions.fetchItems.completed({
-                            hosted_party: data
-                        });
-                    });
-                },
-                icon: 'ban',
-                className: 'info',
-                available: (
-                    party_id !== undefined
-                    && userHasRole(user, 'dm')
-                ),
-            } : {
-                label: 'Host',
-                action: () => {
-                    fetch("/party/host/" + party_id, {
-                        method: "POST",
-                        credentials: 'same-origin',
-                        'headers': {
-                            'X-Requested-With': 'XMLHttpRequest'
-                        }
-                    })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        ListDataActions.fetchItems.completed({
-                            hosted_party: data
-                        });
-                    });
-                },
-                icon: 'beer',
-                available: (
-                    party_id !== undefined
-                    && userHasRole(user, 'dm')
-                ),
-            }),
-            'new': () => ({
-                label: 'New',
-                link: "/party/new",
-                icon: 'plus',
-                available: (
-                    party_id === undefined
-                    && userHasRole(user, ['dm', 'admin'])
-                ),
-            }),
-            'delete': () => ({
-                label: 'Delete',
-                action: () => {
-                    ObjectDataActions.deleteObject(
-                        "party", party_id
-                    );
-                },
-                icon: 'trash-o',
-                className: 'bad',
-                available: (
-                    party_id !== undefined
-                    && userHasRole(user, ['dm', 'admin'])
-                    && user_id == user.id
-                    && !member_ids.length
-                ),
-            }),
-        };
+        return (
+            <BaseLinkGroup {...props}>
+                <BaseLinkButton
+                    name="view"
+                    label="View"
+                    icon="eye"
+                    altStyle={altStyle}
+                    link={`/party/show/${id}`}
+                    available={(
+                        id !== null
+                        && userHasRole(currentUser, ['dm', 'admin'])
+                    )}
+                />
+                <BaseLinkButton
+                    name="raw"
+                    label="Raw"
+                    icon="cogs"
+                    altStyle={altStyle}
+                    download={`/party/raw/${id}`}
+                    available={(
+                        id !== null
+                        && userHasRole(currentUser, 'admin')
+                    )}
+                />
+                <BaseLinkButton
+                    name="edit"
+                    label="Edit"
+                    icon="pencil"
+                    altStyle={altStyle}
+                    link={`/party/edit/${id}`}
+                    available={(
+                        id !== null
+                        && userId === currentUser.id
+                        && userHasRole(currentUser, ['dm', 'admin'])
+                    )}
+                />
+                <BaseLinkButton
+                    name="host"
+                    label="Host"
+                    icon="beer"
+                    className="info"
+                    altStyle={altStyle}
+                    action={this.onHost}
+                    available={(
+                        id !== null
+                        && (
+                            !hostedParty
+                            || hostedParty.id !== id
+                        )
+                        && userHasRole(currentUser, 'dm')
+                    )}
+                />
+                <BaseLinkButton
+                    name="host"
+                    label="Stop"
+                    icon="ban"
+                    className="warning"
+                    altStyle={altStyle}
+                    action={this.onUnhost}
+                    available={(
+                        id !== null
+                        && hostedParty
+                        && hostedParty.id === id
+                        && userHasRole(currentUser, 'dm')
+                    )}
+                />
+                <BaseLinkButton
+                    name="new"
+                    label="New"
+                    icon="plus"
+                    link="/party/new"
+                    altStyle={altStyle}
+                    available={(
+                        id === null
+                        && userHasRole(currentUser, 'dm')
+                    )}
+                />
+                <BaseLinkButton
+                    name="delete"
+                    label="Delete"
+                    icon="trash-o"
+                    className="bad"
+                    action={this.onDelete}
+                    altStyle={altStyle}
+                    available={(
+                        id !== null
+                        && userId === currentUser.id
+                        && userHasRole(currentUser, ['dm', 'admin'])
+                    )}
+                />
+                {children}
+            </BaseLinkGroup>
+        );
     }
-}
+};
 
-
-PartyLinks.propTypes = _.assign(
-    {}, BaseLinkGroup.propTypes, {
-        buttons: PropTypes.arrayOf(
-            PropTypes.oneOf([
-                'view', 'edit', 'new', 'host', 'delete',
-            ])
+PartyLinks.propTypes = {
+    altStyle: PropTypes.bool,
+    id: PropTypes.number,
+    party: PropTypes.shape({
+        user_id: PropTypes.number,
+    }),
+    hosted_party: PropTypes.shape({
+        id: PropTypes.number,
+    }),
+    currentUser: PropTypes.shape({
+        id: PropTypes.number,
+        role: PropTypes.arrayOf(
+            PropTypes.oneOf(['player', 'dm', 'admin'])
         ),
-        party_id: PropTypes.number,
-        current_user: PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            role: PropTypes.arrayOf(
-                PropTypes.oneOf(['player', 'dm', 'admin'])
-            ),
-        }),
-        hosted_party: PropTypes.shape({
-            id: PropTypes.number.isRequired,
-        }),
-    }
-);
+    }),
+};
+
+PartyLinks.defaultProps = {
+    altStyle: false,
+    id: null,
+    party: {},
+    hostedParty: null,
+    currentUser: {},
+};
 
 export default ListDataWrapper(
     ObjectDataWrapper(PartyLinks, [
-        {type: 'party', id: 'party_id'}
+        {type: 'party', id: 'id'}
     ]),
-    ['current_user', 'hosted_party']
+    ['current_user', 'hosted_party'],
+    null,
+    {
+        current_user: 'currentUser',
+        hosted_party: 'hostedParty',
+    }
 );
