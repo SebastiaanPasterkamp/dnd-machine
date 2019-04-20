42a780c25beffc3f534e5b031ce375fc50a28f9f
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-04-20 22:35:41 +0200

    UI: add CharacterPicker using
    BaseItemPicker.

---
+++ ui/src/jsx/components/CharacterPicker/CharacterPicker.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import BaseFilterListDialog from '../BaseFilterListDialog';
+import CharacterRow from './components/CharacterRow.jsx';
+import InputField from '../InputField.jsx';
+import ObjectDataListWrapper from '../../hocs/ObjectDataListWrapper.jsx';
+
+import './sass/_character-picker.scss';
+
+
+export class CharacterPicker extends React.Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPick: null,
+            search: '',
+        };
+    }
+
+    onDone = () => {
+        const { onDone } = this.props;
+        const { currentPick } = this.state;
+        onDone(currentPick);
+    }
+
+    onFilter = (character) => {
+        const { onFilter } = this.props;
+        const { search } = this.state;
+        if (onFilter !== undefined
+            && !onFilter(character, search)
+        ) {
+            return false;
+        }
+
+        if (!search) {
+            return true;
+        }
+
+        const pattern = search.toLowerCase();
+        return (
+            character.name.toLowerCase().match(pattern)
+            || character.class.toLowerCase().match(pattern)
+            || character.race.toLowerCase().match(pattern)
+            || character.background.toLowerCase().match(pattern)
+        );
+    }
+
+    onPick = (currentPick) => {
+        this.setState({ currentPick });
+    }
+
+    onSearch = (search) => this.setState({
+        search: search || '',
+    });
+
+    render() {
+        const {
+            characters,
+            onCancel,
+            onDone,
+            onFilter,
+            ...props
+        } = this.props
+        const { currentPick, search } = this.state;
+
+        return (
+            <BaseFilterListDialog
+                items={characters}
+                label="Pick a Character"
+                className="character-picker"
+                rowComponent={CharacterRow}
+                onCancel={onCancel}
+                onDone={this.onDone}
+                onFilter={this.onFilter}
+                onPick={this.onPick}
+                currentPick={currentPick}
+                {...props}
+            >
+                <InputField
+                    placeholder="Filter..."
+                    value={search}
+                    setState={this.onSearch}
+                />
+            </BaseFilterListDialog>
+        );
+    }
+};
+
+CharacterPicker.propTypes = {
+    onDone: PropTypes.func.isRequired,
+    onFilter: PropTypes.func,
+    onCancel: PropTypes.func,
+    characters: PropTypes.object,
+};
+
+CharacterPicker.defaultProps = {
+    characters: {},
+    onCancel: null,
+};
+
+export default ObjectDataListWrapper(
+    CharacterPicker,
+    {characters: {type: 'character'}}
+);
