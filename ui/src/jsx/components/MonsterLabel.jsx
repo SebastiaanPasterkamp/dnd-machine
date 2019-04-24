34067568023cc318e0f1be889ff67fc1f384f556
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-04-24 23:07:00 +0200

    UI: Improve MonsterLabel layout

--- ui/src/jsx/components/MonsterLabel.jsx
+++ ui/src/jsx/components/MonsterLabel.jsx
@@ -1,64 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ChallengeRating from '../components/ChallengeRating.jsx';
 import ListDataWrapper from '../hocs/ListDataWrapper.jsx';
-import ObjectDataWrapper from '../hocs/ObjectDataWrapper.jsx';
-
-import LazyComponent from '../components/LazyComponent.jsx';
 import ListLabel from '../components/ListLabel.jsx';
+import ObjectDataWrapper from '../hocs/ObjectDataWrapper.jsx';
+import XpRating from '../components/XpRating.jsx';
 
-export class MonsterLabel extends LazyComponent
-{
-    renderType(spell) {
-        const {
-            monster, showType, size_hit_dice = [],
-            monster_types = [], alignments = [],
-        } = this.props;
 
-        if (!showType) {
-            return null;
-        }
+import '../../sass/_monster-label.scss';
 
-        return <span>
-            <ListLabel
-                items={size_hit_dice}
-                value={monster.size}
+export const MonsterLabel = function({
+    monster, showType, size_hit_dice, monster_types, alignments,
+}) {
+    if (!monster) {
+        return null;
+    }
+
+    return (
+        <div className="monster-label inline">
+            <span className="monster-label--name">
+                {monster.name}
+                &nbsp;
+                (<ChallengeRating
+                    challengeRating={monster.challenge_rating}
                 />
-            &nbsp;
-            <ListLabel
-                items={monster_types}
-                value={monster.type}
+                &nbsp;/&nbsp;
+                <XpRating
+                    xpRating={monster.xp}
+                />)
+            </span>
+            <span className="monster-label--type">
+                <ListLabel
+                    items={size_hit_dice}
+                    value={monster.size}
                 />
-            &nbsp;
-            (<ListLabel
+                &nbsp;
+                <ListLabel
+                    items={monster_types}
+                    value={monster.type}
+                />
+                &nbsp;
+                (<ListLabel
                     items={alignments}
                     value={monster.alignment}
-                    />)
-        </span>;
-    }
-
-    render() {
-        const { monster } = this.props;
-
-        if (!monster) {
-            return null;
-        }
-
-        return <div className="monster inline">
-            {monster.name},
-            CR {monster.challenge_rating}
-            {this.renderType()}
-        </div>;
-    }
-}
+                />)
+            </span>
+        </div>
+    )
+};
 
 MonsterLabel.defaultProps = {
     showType: false,
+    monster: null,
+    size_hit_dice: [],
+    monster_types: [],
+    alignments: [],
 };
 
 MonsterLabel.propTypes = {
     showType: PropTypes.bool,
     monster: PropTypes.object,
+    size_hit_dice: PropTypes.array,
+    monster_types: PropTypes.array,
+    alignments: PropTypes.array,
 }
 
 export default ListDataWrapper(
