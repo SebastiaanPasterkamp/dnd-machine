27e7bddec961b7959f09b8f63ca948cb26a556dd
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-04-26 22:36:19 +0200

    UI: CharacterPicker test coverage

---
+++ ui/src/jsx/components/CharacterPicker/__tests__/CharacterRow.react-test.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+import MockRouter from 'react-mock-router';
+
+import CharacterRow from '../components/CharacterRow.jsx';
+
+describe('CharacterRow', () => {
+    const fullProps = {
+        onPick: jest.fn(),
+        item: {
+            id: 1,
+            user_id: 2,
+            race: 'Human',
+            class: 'Fighter',
+            background: 'Soldier',
+            name: 'Testy McTestface',
+            level: 1,
+            gender: 'male',
+            alignment: 'true neutral',
+            adventure_checkpoints: 3,
+            acp_progress: 3,
+            acp_level: 4,
+        },
+        currentPick: 1,
+        label: "Choose",
+        icon: "user",
+    };
+
+    describe('rendering', () => {
+        it('should work with minimum props', () => {
+            const tree = renderer.create(
+                <MockRouter>
+                    <CharacterRow
+                        onPick={fullProps.onPick}
+                        item={fullProps.item}
+                    />
+                </MockRouter>
+            );
+
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should work with full props', () => {
+            const tree = renderer.create(
+                <MockRouter>
+                    <CharacterRow
+                        {...fullProps}
+                    />
+                </MockRouter>
+            );
+
+            expect(tree).toMatchSnapshot();
+        });
+    });
+
+    it('should emit item.id onPick', () => {
+        const wrapper = mount(
+            <MockRouter>
+                <CharacterRow
+                    {...fullProps}
+                />
+            </MockRouter>
+        );
+
+        wrapper
+            .find('.fa-user')
+            .simulate('click');
+
+        expect(fullProps.onPick)
+            .toBeCalledWith(fullProps.item.id);
+    });
+});
