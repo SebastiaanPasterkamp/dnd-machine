31634242dc73f34da28da4985a7fe8b7556489d7
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-05-23 21:06:14 +0200

    UI: Update ReachLabel to display
    feet+inch from various input.

--- ui/tests/components/Reach.react-test.jsx
+++ ui/tests/components/Reach.react-test.jsx
@@ -2,40 +2,100 @@ import React from 'react';
 import Reach from '../../src/jsx/components/Reach.jsx';
 import renderer from 'react-test-renderer';
 
-describe('Component: Reach', () => {
-    it('should show simple number with unit', () => {
-        const props = {distance: 10};
+describe('Reach', () => {
+    it('should render nothing without props', () => {
         const tree = renderer.create(
-            <Reach {...props} />
+            <Reach />
         ).toJSON();
 
         expect(tree).toMatchSnapshot();
     });
 
-    it('should show min<max range with unit', () => {
-        const props = {min: 10, max: 20};
-        const tree = renderer.create(
-            <Reach {...props} />
-        ).toJSON();
+    describe('with min/max', () => {
+        it('should show full range with units when min<max', () => {
+            const tree = renderer.create(
+                <Reach min={10} max={20} />
+            ).toJSON();
 
-        expect(tree).toMatchSnapshot();
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should show min with unit when min==max', () => {
+            const tree = renderer.create(
+                <Reach min={5} max={5} />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should show min with unit when min>max', () => {
+            const tree = renderer.create(
+                <Reach min={5} max={0} />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
     });
 
-    it('should show a simple number for min==max with unit', () => {
-        const props = {min: 5, max: 5};
-        const tree = renderer.create(
-            <Reach {...props} />
-        ).toJSON();
+    describe('with non-numeric distance', () => {
+        it('should show just a text label', () => {
+            const tree = renderer.create(
+                <Reach distance="touch" />
+            ).toJSON();
 
-        expect(tree).toMatchSnapshot();
+            expect(tree).toMatchSnapshot();
+        });
     });
 
-    it('should show just a text label for a non-numeric distance', () => {
-        const props = {distance: 'touch'};
-        const tree = renderer.create(
-            <Reach {...props} />
-        ).toJSON();
+    describe('with explicit feet and/or inch', () => {
+        it('should render only feet', () => {
+            const tree = renderer.create(
+                <Reach feet={1} />
+            ).toJSON();
 
-        expect(tree).toMatchSnapshot();
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should render only inches', () => {
+            const tree = renderer.create(
+                <Reach inch={2} />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should render both', () => {
+            const tree = renderer.create(
+                <Reach feet={3} inch={4} />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
+    });
+
+    describe('with computed feet and/or inch', () => {
+        it('should render only feet', () => {
+            const tree = renderer.create(
+                <Reach distance={1} />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should render only inches', () => {
+            const tree = renderer.create(
+                <Reach distance={2/12} />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should render both', () => {
+            const tree = renderer.create(
+                <Reach distance={10/3} />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
     });
 });
