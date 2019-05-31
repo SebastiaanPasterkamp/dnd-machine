8baee350e23d7afc5e82bf051dfb3d1ddb80e063
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-05-31 22:19:49 +0200

    UI: Defer resp. for cach/refresh to store

--- ui/src/jsx/stores/ObjectDataStore.jsx
+++ ui/src/jsx/stores/ObjectDataStore.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Reflux from 'reflux';
-import _ from 'lodash';
+import {
+    defer,
+    every,
+    forEach,
+    filter,
+    get,
+    isEqual,
+    omit,
+} from 'lodash/fp';
 
 import ObjectDataActions from '../actions/ObjectDataActions.jsx';
+import LoadingActions from '../actions/LoadingActions.jsx';
+import ReportingActions from '../actions/ReportingActions.jsx';
 
 export function ObjectDataStoreFactory(id, listenables = null)
 {
@@ -13,90 +23,236 @@ export function ObjectDataStoreFactory(id, listenables = null)
         {
             super();
             this.state = {
-                timestamp: {}
+                timestamp: {},
+                loading: {},
             };
             this.listenables = listenables || ObjectDataActions;
         }
 
         updateObject(type, id, data, callback=null) {
             const {
-                [type]: typeState,
+                [type]: typeState = {},
                 timestamp,
+                loading,
             } = this.state;
 
-            if (_.isEqual(data, _.get(typeState, id))) {
+            const state = {
+                timestamp: {
+                    ...timestamp,
+                    [`${type}.${id}`]: Date.now(),
+                },
+                loading: {
+                    ...loading,
+                    [`${type}.${id}`]: false,
+                },
+            };
+
+            if (isEqual(data, typeState[id])) {
+                this.setState(state);
                 if (callback) {
-                    callback(type, id, data);
+                    callback(type, id, typeState[id]);
                 }
+                return;
+            }
+
+            state[type] = {
+                ...typeState,
+                [id]: data,
+            };
+            this.setState(state);
+            if (callback) {
+                defer(() => callback(type, id, data));
+            }
+        };
+
+        onListObjects(type, group=null, callback=null) {
+            const {
+                loading: {
+                    [type]: loading,
+                },
+                timestamp: {
+                    [type]: timestamp,
+                },
+            } = this.state;
+            const maxAge = Date.now() - 60.0 * 1000.0;
+            if ( loading || timestamp >= maxAge ) {
                 return;
             }
 
             this.setState({
-                timestamp: _.assign(
-                    {},
-                    timestamp,
-                    {[type]: _.assign(
-                        {},
-                        timestamp[type],
-                        {[id]: Date.now()}
-                    )}
-                ),
-                [type]: _.assign(
-                    {},
-                    typeState,
-                    {[id]: data}
-                )
+                loading: {
+                    ...loading,
+                    [type]: true,
+                },
             });
+            LoadingActions.start(type);
 
-            if (callback) {
-                _.defer(() => callback(type, id, data));
-            }
-        };
+            const path = '/' + filter(null, [
+                group, type, 'api'
+            ]).join('/');
+
+            fetch(path, {
+                credentials: 'same-origin',
+                method: 'GET',
+                'headers': {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response.statusText;
+            })
+            .then((result) => {
+                LoadingActions.finish(type);
+                this.listenables.listObjects.completed(
+                    type, result, callback
+                );
+            })
+            .catch((error) => {
+                LoadingActions.finish(type);
+                console.log(error);
+                this.listenables.listObjects.failed(
+                    type, error
+                );
+                ReportingActions.showMessage(
+                    'bad',
+                    error.message,
+                    'Fetch list'
+                );
+            });
+        }
 
         onListObjectsCompleted(type, objects, callback=null) {
-            let update = {
-                timestamp: {}
+            const {
+                [type]: typeState = {},
+                timestamp,
+                loading,
+            } = this.state;
+
+            const update = {
+                typeState: {},
+                timestamp: {
+                    [type]: Date.now(),
+                },
+                loading: {
+                    [type]: false,
+                },
             };
 
-            update = _.reduce(objects, (mapped, object) => {
-                let old_object = _.get(
-                        this.state,
-                        [type, object.id]
-                    ),
-                    old_timestamp = _.get(
-                        this.state.timestamp,
-                        [type, object.id]
-                    );
-
-                if (old_object && _.isEqual(object, old_object)) {
-                    mapped[type][object.id] = old_object;
-                    mapped.timestamp[type][object.id] = old_timestamp;
-                } else {
-                    mapped[type][object.id] = object;
-                    mapped.timestamp[type][object.id] = Date.now();
+            forEach((object) => {
+                const { id } = object;
+                update.timestamp[`${type}.${id}`] = Date.now();
+                update.loading[`${type}.${id}`] = false;
+                if (!isEqual(object, typeState[id])) {
+                    update.typeState[id] = object;
                 }
+            })(objects);
 
-                return mapped;
-            }, {
-                timestamp: {[type]: {}},
-                [type]: {}
+            this.setState({
+                timestamp: {
+                    ...timestamp,
+                    ...update.timestamp,
+                },
+                loading: {
+                    ...loading,
+                    ...update.loading,
+                },
+                [type]: {
+                    ...typeState,
+                    ...update.typeState,
+                },
+            }, callback);
+        }
+
+        onListObjectsFailed(type) {
+            const { loading } = this.state;
+            this.setState({
+                loading: {
+                    ...loading,
+                    [type]: false,
+                },
             });
+        }
 
-            if (_.isEqual(update[type], _.get(this.state, type))) {
+        onGetObject(type, id, group=null, callback=null) {
+            const {
+                loading,
+                timestamp: {
+                    [`${type}.${id}`]: timeObject = 0,
+                },
+            } = this.state;
+            const {
+                [type]: loadingList,
+                [`${type}.${id}`]: loadingObject,
+            } = loading;
+            const maxAge = Date.now() - 60.0 * 1000.0;
+            if (
+                loadingList
+                || loadingObject
+                || timeObject >= maxAge
+            ) {
                 return;
             }
 
-            update.timestamp = _.assign(
-                {},
-                this.state.timestamp,
-                update.timestamp
-            );
+            this.setState({
+                loading: {
+                    ...loading,
+                    [`${type}.${id}`]: true,
+                },
+            });
+            LoadingActions.start(type);
 
-            this.setState(update, callback);
+            const path = '/' + filter(null, [
+                group, type, 'api', id
+            ]).join('/');
+
+            fetch(path, {
+                credentials: 'same-origin',
+                method: 'GET',
+                'headers': {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response.statusText;
+            })
+            .then((result) => {
+                LoadingActions.finish(type);
+                this.listenables.getObject.completed(
+                    type, id, result, callback
+                );
+            })
+            .catch((error) => {
+                LoadingActions.finish(type);
+                console.log(error);
+                this.listenables.getObject.failed(
+                    type, id, error
+                );
+                ReportingActions.showMessage(
+                    'bad',
+                    error.message,
+                    'Fetch object'
+                );
+            });
         }
 
         onGetObjectCompleted(type, id, object, callback=null) {
             this.updateObject(type, id, object, callback);
+        }
+
+        onGetObjectFailed(type, id) {
+            const { loading } = this.state;
+            this.setState({
+                loading: {
+                    ...loading,
+                    [`${type}.${id}`]: false,
+                },
+            });
         }
 
         onPatchObjectCompleted(type, id, object, callback=null) {
@@ -105,10 +261,10 @@ export function ObjectDataStoreFactory(id, listenables = null)
 
         onPostObjectCompleted(type, id, object, callback=null) {
             const { [type]: items } = this.state;
-            this.setState({
-                [type]: _.omit(items, null),
-            }, callback);
             this.updateObject(type, id, object, callback);
+            this.setState({
+                [type]: omit(null, items),
+            }, callback);
         }
 
         onConsumeObjectCompleted(type, id, object, callback=null) {
@@ -132,7 +288,7 @@ export function ObjectDataStoreFactory(id, listenables = null)
                 return;
             }
             this.setState({
-                [type]: _.omit(items, id),
+                [type]: omit(id, items),
             }, callback);
         }
     }
