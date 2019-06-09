bc5f8e99b540ade285eb33661892d590869dc1f3
sebastiaan.pasterkamp+github@gmail.com <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-06-09 16:32:01 +0200

    UI: Only import minimum math.js part

--- ui/src/jsx/utils.jsx
+++ ui/src/jsx/utils.jsx
@@ -1,5 +1,55 @@
 import _ from 'lodash';
-import math from 'mathjs';
+import core from 'mathjs/core';
+const math = core.create();
+math.import(require('mathjs/lib/expression/function/eval'));
+
+// create simple functions for all operators
+math.import({
+    // arithmetic
+    add:        function (a, b) { return a + b },
+    subtract:   function (a, b) { return a - b },
+    multiply:   function (a, b) { return a * b },
+    divide:     function (a, b) { return a / b },
+    mod:        function (a, b) { return a % b },
+    unaryPlus:  function (a) { return  a },
+    unaryMinus: function (a) { return -a },
+
+    // bitwise
+    bitOr:           function (a, b) { return a | b },
+    bitXor:          function (a, b) { return a ^ b },
+    bitAnd:          function (a, b) { return a & b },
+    bitNot:          function (a) { return ~a },
+    leftShift:       function (a, b) { return a << b },
+    rightArithShift: function (a, b) { return a >> b },
+    rightLogShift:   function (a, b) { return a >>> b },
+
+    // logical
+    or:  function (a, b) { return !!(a || b) },
+    xor: function (a, b) { return !!a !== !!b },
+    and: function (a, b) { return !!(a && b) },
+    not: function (a) { return !a },
+
+    // relational
+    equal:     function (a, b) { return a == b },
+    unequal:   function (a, b) { return a != b },
+    smaller:   function (a, b) { return a < b },
+    larger:    function (a, b) { return a > b },
+    smallerEq: function (a, b) { return a <= b },
+    largerEq:  function (a, b) { return a >= b },
+    pi: Math.PI,
+    e: Math.E,
+    'true': true,
+    'false': false,
+    'null': null,
+});
+
+var allFromMath = {};
+Object.getOwnPropertyNames(Math).forEach(function (name) {
+    if (!Object.prototype.hasOwnProperty(name)) {
+        allFromMath[name] = Math[name];
+    }
+});
+math.import(allFromMath);
 
 const styleCache = {
     '': null,
@@ -64,9 +114,11 @@ const utils = {
                 replace[match] = JSON.stringify(value);
             }
         });
+
         _.forEach(replace, (replace, match) => {
             formula = _.replace(formula, match, replace);
         });
+
         try {
             const result = math.eval(formula);
             if (result.toArray !== undefined) {
