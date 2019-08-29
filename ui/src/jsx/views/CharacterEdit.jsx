ed33608490a42797aedf9fba5e5a5a712f7293e1
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2019-08-29 20:30:36 +0200

    UI: Char Edit improve spell prepping

--- ui/src/jsx/views/CharacterEdit.jsx
+++ ui/src/jsx/views/CharacterEdit.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+    entries,
     filter,
+    flow,
     isEmpty,
     map,
 } from 'lodash/fp';
@@ -67,11 +69,14 @@ export class CharacterEdit extends React.Component
             'class': _class,
         } = character;
         const levelFilter = filter(
-            map((slot, count) => count
+            null,
+            flow(entries, map(([slot, count]) => count
                 ? slot.replace('level_', '')
                 : null
-            )(slots)
+            ))(slots)
         );
+
+        console.log({ slots, levelFilter });
 
         return (
             <React.Fragment>
@@ -110,12 +115,12 @@ export class CharacterEdit extends React.Component
                     />
                 </Panel>
 
-                {max_prepared ? <Panel
+                {max_prepared || list.length || cantrips.length ? <Panel
                     key="prepared"
                     className="character-edit__prepared"
                     header="Spells Prepared"
                 >
-                    <CharacterConfig
+                    {max_prepared ? <CharacterConfig
                         config={[{
                             "label": "Prepared spells",
                             "path": "spell.prepared",
@@ -126,14 +131,32 @@ export class CharacterEdit extends React.Component
                             "filter": {
                                 "or": [{
                                     "classes": _class,
-                                    "level": levelFilter
+                                    "level": levelFilter,
                                 }, {
-                                    "name": expanded
+                                    "name": expanded,
                                 }],
+                                "not": {
+                                    "name": list,
+                                },
                             },
-                            "given": list,
                         }]}
-                    />
+                    /> : null }
+                    {cantrips.length ? <CharacterConfig
+                        config={[{
+                            "label": "Known cantrips",
+                            "path": "spell.cantrips",
+                            "type": "list",
+                            "list": ["spell"],
+                        }]}
+                    /> : null}
+                    {list.length ? <CharacterConfig
+                        config={[{
+                            "label": "Known spells",
+                            "path": "spell.list",
+                            "type": "list",
+                            "list": ["spell"],
+                        }]}
+                    /> : null }
                 </Panel> : null}
 
                 <Panel
