beeee670ac4d2564cc4de7097c7277c031db179f
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2020-03-17 22:34:48 +0100

    UI: Fix SubClassEdit

--- ui/src/jsx/views/SubClassEdit/SubClassEdit.jsx
+++ ui/src/jsx/views/SubClassEdit/SubClassEdit.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+    find,
     map,
     range,
 } from 'lodash/fp';
@@ -19,36 +20,111 @@ import Panel from '../../components/Panel';
 import SingleSelect from '../../components/SingleSelect';
 import TabComponent from '../../components/TabComponent';
 
+import {
+    DataConfig,
+    ListConditions,
+    PhasePanel,
+    uuidv4,
+} from '../../components/DataConfig';
+
 export class SubClassEdit extends React.Component
 {
+    optionType = 'config';
+
     constructor(props) {
         super(props);
-
-        this.levels = map(i => ({
-            name: `Level ${i}`,
-            level: i,
-        }))(range(2, 20));
-
-        this.onFieldChange = this.onFieldChange.bind(this);
-
+        const {
+            class_id,
+            classes,
+            uuid = uuidv4(),
+        } = props;
+        const {
+            subclass_level = 1,
+        } = find({id: class_id}, classes) || {};
+        this.state = {
+            uuid,
+            levels: map(
+                (level) => ({ name: `${level}` })
+            )(range(subclass_level, 20)),
+            subclass_level,
+        };
         this.memoize = memoize.bind(this);
+    }
+
+    initPhase(level) {
+        return this.memoize(`init-level-${level}`, ({name = ''}) => {
+            if (name !== '') {
+                return null;
+            }
+            const { name: subclass } = this.props;
+
+            return ({
+                name: `${subclass} ${level}`,
+                conditions: [
+                    {path: 'subclass', type: 'contains', needle: subclass},
+                    {path: 'level', type: 'gte', value: level },
+                ],
+            });
+        });
     }
 
     onFieldChange(field) {
         return this.memoize(field, value => {
-            const { setState } = this.props;
-            setState({ [field]: value });
+            const { uuid: stateUUID } = this.state;
+            const { setState, uuid = stateUUID } = this.props;
+            setState({
+                type: this.optionType,
+                uuid,
+                [field]: value,
+            });
         });
     }
 
-    render() {
+    onPhaseChange(index) {
+        const padding = map(() => ({}))(range(2, 20));
+        return this.memoize(`phase-${index}`, phase => {
+            const { uuid: stateUUID } = this.state;
+            const { setState, phases, uuid = stateUUID } = this.props;
+            const updated = 'config' in phase && !phase.config.length
+                ? null
+                : {...phases[index], ...phase};
+            setState({
+                type: this.optionType,
+                uuid,
+                phases: [
+                    ...[...phases, ...padding].slice(0, index),
+                    updated,
+                    ...phases.slice(index + 1),
+                ],
+            });
+        });
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        const { class_id, classes } = props;
         const {
-            id,
-            class: _class,
-            subclass,
-            description,
-            classes,
-            weapon,
+            subclass_level = 1,
+        } = find({id: class_id}, classes) || {};
+        return {
+            levels: [
+                { name: `${props.name} ${subclass_level}` },
+                ...map(
+                    (index) => {
+                        const {
+                            name = `${index + 1 + subclass_level}`,
+                        } = props.phases[index] || {};
+                        return { name };
+                    }
+                )(range(0, 20 - subclass_level)),
+            ],
+            subclass_level,
+        };
+    }
+
+    render() {
+        const { levels, subclass_level } = this.state;
+        const {
+            id, class_id, name, description, config, phases, conditions, classes,
         } = this.props;
 
         return (
@@ -60,17 +136,17 @@ export class SubClassEdit extends React.Component
                 >
                     <ControlGroup label="Class">
                         <SingleSelect
-                            selected={_class}
-                            items={classes.length ? classes[0].options : []}
-                            setState={this.onFieldChange('class')}
+                            selected={class_id}
+                            items={classes}
+                            setState={this.onFieldChange('class_id')}
                         />
                     </ControlGroup>
 
                     <ControlGroup label="Sub Class">
                         <InputField
                             placeholder="Sub Class..."
-                            value={subclass}
-                            setState={this.onFieldChange('subclass')}
+                            value={name}
+                            setState={this.onFieldChange('name')}
                         />
                     </ControlGroup>
 
@@ -84,10 +160,14 @@ export class SubClassEdit extends React.Component
                 </Panel>
 
                 <Panel
-                    key="requirements"
-                    className="sub-class-edit__requirements"
-                    header="Requirements"
+                    key="conditions"
+                    className="sub-class-edit__conditions"
+                    header="Conditions"
                 >
+                    <ListConditions
+                        conditions={conditions}
+                        setState={this.onFieldChange('conditions')}
+                    />
                 </Panel>
 
                 <Panel
@@ -96,9 +176,21 @@ export class SubClassEdit extends React.Component
                     header="Levels"
                 >
                     <TabComponent
-                        tabConfig={this.tabConfig}
+                        tabConfig={levels}
                         mountAll={true}
                     >
+                        <DataConfig
+                            list={config}
+                            setState={this.onFieldChange('config')}
+                        />
+                        {map(
+                            (index) =>(<PhasePanel
+                                key={`phase-${index}`}
+                                initPhase={this.initPhase(index + 1 + subclass_level)}
+                                {...phases[index]}
+                                setState={this.onPhaseChange(index)}
+                            />)
+                        )(range(0, levels.length-1))}
                     </TabComponent>
                 </Panel>
             </React.Fragment>
@@ -108,40 +200,39 @@ export class SubClassEdit extends React.Component
 
 SubClassEdit.propTypes = {
     id: PropTypes.number,
-    class: PropTypes.string,
-    subclass: PropTypes.string,
+    class_id: PropTypes.number,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    config: PropTypes.arrayOf(PropTypes.object),
+    phases: PropTypes.arrayOf(PropTypes.object),
     description: PropTypes.string,
     classes: PropTypes.array,
-    weapon: PropTypes.array,
 };
 
 SubClassEdit.defaultProps = {
-    class: '',
-    subclass: '',
+    class_id: null,
+    name: '',
     description: '',
+    config: [],
+    phases: [],
     classes: [],
-    weapon: [],
 };
 
 export default ListDataWrapper(
-    ListDataWrapper(
-        RoutedObjectDataWrapper(
-            SubClassEdit,
-            {
-                className: 'sub-class-edit',
-                icon: 'fa-users',
-                label: 'Sub Class',
-                buttons: ['cancel', 'save']
-            },
-            "subclass"
-        ),
-        [
-            'weapon',
-        ],
-        'items',
+    RoutedObjectDataWrapper(
+        SubClassEdit,
+        {
+            className: 'sub-class-edit',
+            icon: 'fa-users',
+            label: 'Sub Class',
+            buttons: ['cancel', 'save']
+        },
+        "subclass",
+        "data"
     ),
-    [
-        'classes',
-    ],
-    'character'
+    ['class'],
+    'data',
+    {
+        'class': 'classes',
+    },
 );
