e26f14409765aae61da5bd36c668f245543a936a
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2020-09-10 22:08:04 +0200

    App: Remove character 'weapon' attribute

--- app/models/character.py
+++ app/models/character.py
@@ -63,7 +63,6 @@ class CharacterObject(JsonObject):
         "challenge": {},
         "skills": {},
         "equipment": [],
-        "weapon": [],
         "armor": [],
         "items": {
             "artisan": [],
