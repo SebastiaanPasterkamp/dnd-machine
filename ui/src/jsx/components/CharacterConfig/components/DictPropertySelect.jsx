6b7b5f36c3048637d536b4af32fe2049534e226e
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2020-09-10 20:07:33 +0200

    UI: Char. Config derives 'default'
    values from _default keys automatically.

--- ui/src/jsx/components/CharacterConfig/components/DictPropertySelect.jsx
+++ ui/src/jsx/components/CharacterConfig/components/DictPropertySelect.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { sprintf } from 'sprintf-js';
 import MDReactComponent from 'markdown-react-js';
+import {
+    keys,
+    reduce,
+} from 'lodash/fp';
 
 import CharacterEditorWrapper from '../hocs/CharacterEditorWrapper';
 
@@ -12,13 +16,26 @@ export const DictPropertySelect = function({ hidden, current })
     }
 
     let value = current.description;
+    const defaulted = reduce(
+        (defaulted, key) => {
+            if (!key.match(/_default$/)) {
+                return defaulted;
+            }
+            const field = key.replace(/_default$/, '');
+            return {
+                [field]: current[key],
+                ...defaulted,
+            };
+        },
+        {}
+    )(keys(current));
     try {
         value = sprintf(
             current.description || '',
-            current
+            { ...defaulted, ...current }
         );
     } catch(err) {
-        console.error(err);
+        console.error(err, current);
     }
 
     return (
